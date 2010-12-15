0736687d88ed6079468d40448b62833784425920
phoet <phoetmail@googlemail.com>
2010-12-16 00:38:34 +0100

    remove before_filter from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :load_stuff
-  helper_method :current_user, :signed_in?
+  helper_method :current_user, :signed_in?, :preview_events, :tweets, :random_users
 
   private
 
@@ -18,16 +17,26 @@ class ApplicationController < ActionController::Base
     @current_user = user
     session[:user_id] = user.id
   end
+  
+  def preview_events
+    @preview_events ||= Event.preview_events
+  end
 
-  def load_stuff
-    @tweets = cache(:tweets, :expires_in => 1.minute) do
+  def tweets
+    cache(:tweets, :expires_in => 1.minute) do
       logger.debug "fetching new tweets"
       Twitter::Search.new.q("rails OR ruby OR rughh").geocode(53.561858,9.962021,'100km').rpp(5).fetch
     end
-    @preview_events = Event.preview_events
-    @random_users = cache(:random_users, :expires_in => 1.minute) do
+  rescue
+    []
+  end
+  
+  def random_users
+    cache(:random_users, :expires_in => 1.minute) do
       User.random
     end
+  rescue
+    []
   end
 
 end
