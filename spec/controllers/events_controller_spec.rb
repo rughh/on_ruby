151d9fa93191c227c29cdc3212a53de15581d0ce
phoet <phoetmail@googlemail.com>
2010-12-16 00:38:01 +0100

    use let

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,31 +1,19 @@
 require 'spec_helper'
 
 describe EventsController do
+  let(:mock_data) { 'moooooooooockmockmock' }
 
   def mock_event(stubs={})
     (@mock_event ||= mock_model(Event).as_null_object).tap do |event|
       event.stub(stubs) unless stubs.empty?
     end
   end
-  
-  def mock_data
-    'moooooooooockmockmock'
-  end
-  
+
   before(:each) do
     Event.stub(:preview_events) { [mock_event] }
     @controller.stub(:cache) { [mock_data] }
   end
-  
-  describe "GET info" do
-    it "assigns all preview data" do
-      get :info
-      assigns(:preview_events).should eq([mock_event])
-      assigns(:tweets).should eq([mock_data])
-      assigns(:random_users).should eq([mock_data])
-    end
-  end
-  
+
   describe "GET show" do
     it "assigns the requested event as @event" do
       Event.stub(:find).with("37") { mock_event }
@@ -33,4 +21,5 @@ describe EventsController do
       assigns(:event).should be(mock_event)
     end
   end
+
 end
