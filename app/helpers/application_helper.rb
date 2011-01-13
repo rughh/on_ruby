2ba9259c69ec96895deae4a174377df5a2d4b032
phoet <phoetmail@googlemail.com>
2011-01-13 16:01:56 +0100

    use faraday, where did httparty go?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def repos(nickname)
     logger.info "getting repos for #{nickname}"
-    resp = HTTParty.get("http://github.com/api/v2/json/repos/show/#{nickname}", :type => :json)
-    repos = Hashie::Mash.new(resp).repositories || []
+    resp = Faraday.get "http://github.com/api/v2/json/repos/show/#{nickname}"
+    repos = Hashie::Mash.new(JSON.parse(resp.body)).repositories || []
     repos.sort{|a, b| b.watchers + b.forks <=> a.watchers + a.forks}[0..2]
   rescue
     logger.warn $!
