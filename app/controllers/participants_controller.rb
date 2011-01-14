710c58f0b616d9f10a030413053d7eb0132c458a
phoet <phoetmail@googlemail.com>
2011-01-14 10:59:34 +0100

    test deletion of participants

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,8 +1,15 @@
+# encoding: utf-8
+
 class ParticipantsController < ApplicationController
   def destroy
     participant = Participant.find params[:id]
     event = participant.event
-    participant.destroy
-    redirect_to event_path(event), :notice => 'Du bist vom Event abgemeldet.'
+    if participant.owned_by? current_user
+      participant.destroy
+      redirect_to event_path(event), :notice => 'Du bist vom Event abgemeldet.'
+    else
+      redirect_to event_path(event), :alert => 'Hoppala, diese Anmeldung darfst du nicht löschen!'
+    end
   end
 end
+
--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
 describe ParticipantsController do
+  describe "DELETE :destroy" do
+    before do
+      @participant = Factory(:participant)
+      @event = @participant.event
+      @user = @participant.user
+    end
+
+    it "should delete a participant for current user" do
+      @controller.stub(:current_user => @user)
+      expect { delete(:destroy, :id => @participant.id, :event_id => @event.id) }.to change(Participant, :count).by(-1)
+      response.should redirect_to(@event)
+    end
+
+    it "should delete a participant for another user" do
+      @controller.stub(:current_user => Factory(:user))
+      expect { delete(:destroy, :id => @participant.id, :event_id => @event.id) }.to change(Participant, :count).by(0)
+      response.should redirect_to(@event)
+    end
+  end
 end
+
