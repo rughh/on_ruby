29f34678168000a5fd73fde64c354d410405f234
phoet <phoetmail@googlemail.com>
2011-02-26 14:13:47 +0100

    create menu from helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,38 @@
 module ApplicationHelper
 
-  def repos(nickname)
-    logger.info "getting repos for #{nickname}"
-    resp = Faraday.get "http://github.com/api/v2/json/repos/show/#{nickname}"
-    repos = Hashie::Mash.new(MultiJson.decode(resp.body)).repositories || []
-    repos.sort { |a, b| b.watchers + b.forks <=> a.watchers + a.forks }[0..2]
-  rescue
-    logger.warn $!
-    []
+  Menu = Struct.new :name, :controller, :action
+  MENU_ENTRIES = [
+    Menu.new(:info,     :home,    :info),
+    Menu.new(:events,   :events,  :index),
+    Menu.new(:users,    :users,   :index),
+    Menu.new(:wishes,   :wishes,  :index),
+    Menu.new(:imprint,  :home,    :imprint),
+  ]
+
+  def menu
+    MENU_ENTRIES.map do |item|
+      content_tag :li, :class => css_class_for_menu_item(item) do
+        content_tag(:span) + content_tag(:b) do
+          link_to t("menu.#{item.name}"), :controller => item.controller, :action => item.action
+        end
+      end
+    end.join.html_safe
   end
 
-  def awesome_link_to(name, url, options={})
-    content_tag :li, :class => css_class_for_link(name) do
-      content_tag(:span) + content_tag(:b) { link_to t("menu.#{name}", options), url }
-    end
+  def login
+    content_tag(:div, :class => 'login') do
+      content_tag :li do
+        content_tag(:span) + content_tag(:b) do
+          if current_user
+            link_to t("menu.logout", :name => current_user.nickname), destroy_session_path
+          else
+            link_to t("menu.login"), auth_path
+          end
+        end
+      end
+    end.html_safe
   end
-  
+
   def flash_growl
     if notice
       js add_gritter(flash[:notice], :image => :notice, :title => "Hey:", :time => 2000)
@@ -24,10 +41,10 @@ module ApplicationHelper
     end
   end
 
-private
+  private
 
-  def css_class_for_link(name)
-    controller.controller_name == name.to_s ? "active" : ""
-  end
+    def css_class_for_menu_item(item)
+      item.controller == controller.controller_name.to_sym && item.action == controller.action_name.to_sym ? 'active' : ''
+    end
 
 end
---
+++ db/schema.rb
@@ -0,0 +1,134 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20110120094315) do
+
+  create_table "authorizations", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events", :force => true do |t|
+    t.string   "name"
+    t.datetime "date"
+    t.text     "description"
+    t.integer  "location_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "published"
+  end
+
+  create_table "histories", :force => true do |t|
+    t.string   "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "histories", ["item", "table", "month", "year"], :name => "index_histories_on_item_and_table_and_month_and_year"
+
+  create_table "locations", :force => true do |t|
+    t.string   "name"
+    t.string   "url"
+    t.string   "street"
+    t.string   "house_number"
+    t.string   "city"
+    t.string   "zip"
+    t.float    "lat"
+    t.float    "long"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "materials", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "url"
+    t.string   "material_type"
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "participants", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.boolean  "maybe"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "slugs", :force => true do |t|
+    t.string   "name"
+    t.integer  "sluggable_id"
+    t.integer  "sequence",                     :default => 1, :null => false
+    t.string   "sluggable_type", :limit => 40
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "slugs", ["name", "sluggable_type", "sequence", "scope"], :name => "index_slugs_on_n_s_s_and_s", :unique => true
+  add_index "slugs", ["sluggable_id"], :name => "index_slugs_on_sluggable_id"
+
+  create_table "topics", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "nickname"
+    t.string   "name"
+    t.string   "image"
+    t.string   "url"
+    t.string   "location"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "github"
+    t.boolean  "admin"
+  end
+
+  add_index "users", ["nickname"], :name => "index_users_on_nickname", :unique => true
+
+  create_table "votes", :force => true do |t|
+    t.integer  "wish_id"
+    t.integer  "user_id"
+    t.integer  "count"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "wishes", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "activity"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
