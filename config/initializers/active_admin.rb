93fb3ea88859171587799d080678e6a8df5b736c
phoet <phoetmail@googlemail.com>
2011-06-30 15:44:53 +0200

    cleanup devise admin stuff

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -32,7 +32,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # within the controller.
-  config.authentication_method = :authenticate_no_deveise_admin_user!
+  config.authentication_method = :authenticate_admin_user!
 
 
   # == Current User
