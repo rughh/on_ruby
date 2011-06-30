6ad2d994913660f809404b7b507ef9ddafbade35
phoet <phoetmail@googlemail.com>
2011-06-30 15:44:53 +0200

    cleanup devise admin stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 HamburgOnRuby::Application.routes.draw do
 
-  ActiveAdmin.routes(self)
+  devise_for :users, ActiveAdmin::Devise.config
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
   resources :wishes do
     resources :votes
