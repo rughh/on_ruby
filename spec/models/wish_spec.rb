5caf11cfac82bfe0d461f3112ab82d43933602a8
phoet <phoetmail@googlemail.com>
2011-07-06 11:14:20 +0200

    remove colon from tweet

--- spec/models/wish_spec.rb
+++ spec/models/wish_spec.rb
@@ -11,7 +11,7 @@ describe Wish do
     message = @wish.twitter_message('http://bitly.url')
     message.length.should be < 140
     message.should match "Neuer Wunsch von @uschi"
-    message.should match ": 'The xing mobile website: touch.xing.com' http://bitly.url"
+    message.should match "'The xing mobile website: touch.xing.com' http://bitly.url"
   end
 
   context "with votes" do
