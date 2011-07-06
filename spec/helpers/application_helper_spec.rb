aceab82ed77a99abd81f106950ec94257bc24031
phoet <phoetmail@googlemail.com>
2011-07-06 12:53:13 +0200

    move twitter and github url building to helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,11 @@
 require "spec_helper"
 
 describe ApplicationHelper do
+  
+  before do
+    @user = Factory(:user)
+  end
+  
   describe "#menu" do
     it "should return the HTML for the menu" do
       result = helper.menu
@@ -8,15 +13,15 @@ describe ApplicationHelper do
     end
     
     it "should have an active menu item" do
-      helper.stubs(:controller => OpenStruct.new(:controller_name => 'home', :action_name => 'info'))
+      helper.stubs(:controller => OpenStruct.new(controller_name: 'home', action_name: 'info'))
       result = helper.menu
       result.scan('<li class="active">').size.should == 1
     end
   end
   
   describe "#login" do
-    before(:each) do
-      helper.stubs(:current_user => nil)
+    before do
+      helper.stubs(current_user: nil)
     end
     
     it "should return the HTML for the login-part" do
@@ -30,10 +35,28 @@ describe ApplicationHelper do
     end
     
     it "should show logout link for current_user" do
-      user = Factory(:user)
-      stubs(:current_user => user)
+      stubs(current_user: @user)
       result = helper.login
       result.should match('/auth/destroy_session') # destroy_session_path stopped working!
     end
   end
+  
+  describe "#link_to_github" do
+    it "should generate the link" do
+      helper.link_to_github(@user).should eql('<a href="http://github.com/giddiup">github.com/giddiup</a>')
+    end
+    
+    it "should render nothing for no github" do
+      @user.github = nil
+      helper.link_to_github(@user).should be_nil
+    end
+  end
+  
+  describe "#link_to_twitter" do
+    it "should generate the link" do
+      nickname = @user.nickname
+      helper.link_to_twitter(@user).should eql("<a href=\"http://twitter.com/#{nickname}\">@#{nickname}</a>")
+    end
+  end
+  
 end
