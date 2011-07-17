aa6c1af59bb751c7e27a1dbe3ba4285e900a6581
phoet <phoetmail@googlemail.com>
2011-07-17 12:06:19 +0200

    freelance companies and usergroups

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,19 +5,23 @@ describe UsersController do
   describe "PUT :update" do
     before do
       @user = Factory(:user)
-      @data = {:id => @user.id, :user => {:github => 'testo'}}
+      @data = {id: @user.id, user: { github: 'testo', freelancer: true, available: true }}
       request.env["HTTP_REFERER"] = root_url
     end
 
     it "should update the github attribute of a user" do
-      @controller.stubs(:current_user => @user)
+      @controller.stubs(current_user: @user)
       put :update, @data
-      controller.user.github.should eql('testo')
+      controller.user.tap do |user|
+        user.github.should eql('testo')
+        user.freelancer.should be_true
+        user.available.should be_true
+      end
       response.should redirect_to(:back)
     end
 
     it "should not update injected properties" do
-      @controller.stubs(:current_user => @user)
+      @controller.stubs(current_user: @user)
       @data[:user][:nickname] = 'not_allowed_property'
       put :update, @data
       controller.user.nickname.should eql(@user.nickname)
