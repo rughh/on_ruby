dceffd76d236a3996b1c686ebf718f720922a944
phoet <phoetmail@googlemail.com>
2011-07-19 20:44:48 +0200

    concrete functionality

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,30 +1,40 @@
 function initialize_map() {
-  var element = document.getElementById("map_canvas");
-  if(!element) {
-    return;
-  }
-  var data = $.parseJSON($(element).attr('data-map')).location;  
-  var myOptions = {
-    zoom: 14,
-    center: new google.maps.LatLng(data.lat, data.long),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(element, myOptions);
-  
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(data.lat, data.long),
-    map: map,
-    title: data.name
+  jQuery.each($(".map_canvas"), function() {
+    var element = this;
+    if(!element) {
+      return;
+    }
+    var init = $.parseJSON($(element).attr('data-init'));
+    var myOptions = {
+      zoom: init.zoom,
+      center: new google.maps.LatLng(init.lat, init.long),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(element, myOptions);
+    
+    var arr = $.parseJSON($(element).attr('data-map'));
+    jQuery.each(arr, function() {
+      var data = this.location;
+
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.lat, data.long),
+        map: map,
+        title: data.name
+      });
+      
+      var h = '<h1>' + data.name + "</h1><p>"
+      h += data.street + ' ' + data.house_number + ' ' + ' in ' + data.zip + ' ' + data.city + '</p></p>'
+      h += '<a href="' + data.url + '">' + data.url + '</a></p>'
+      var infowindow = new google.maps.InfoWindow({
+          content: h
+      });
+      
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+      });
+    });
   });
 
-  var infowindow = new google.maps.InfoWindow({
-      content: $('#map_info_content').html()
-  });
-  infowindow.open(map, marker);
-
-  // google.maps.event.addListener(marker, 'click', function() {
-  //   infowindow.open(map, marker);
-  // });
 }
 
 function show_hide() {
