ebf1def2712fdcb35b160c059ad30c2d8a18d513
phoet <phoetmail@googlemail.com>
2011-07-19 20:44:48 +0200

    concrete functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,6 @@ class HomeController < ApplicationController
   expose(:current_event) { Event.current }
   expose(:events) { Event.last(5) }
   expose(:users) { User.all }
+  expose(:whishes) { Wish.all }
+  expose(:locations) { Location.all }
 end
