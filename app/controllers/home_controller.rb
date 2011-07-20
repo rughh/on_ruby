67eb9184a2921dd9a78acd5fb10640ba73407b97
phoet <phoetmail@googlemail.com>
2011-07-20 16:32:52 +0200

    finailze wishes etc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   expose(:current_event) { Event.current }
   expose(:events) { Event.last(5) }
   expose(:users) { User.all }
-  expose(:whishes) { Wish.all }
+  expose(:wishes) { Wish.all }
   expose(:locations) { Location.all }
   expose(:organizers) { User.where(:nickname => ['halfbyte', 'ralph', 'phoet', 'rubiii']) }
 end
