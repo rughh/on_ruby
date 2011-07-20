edd827506ba7cdf798abe6448fed4e5bd3971737
phoet <phoetmail@googlemail.com>
2011-07-20 16:32:52 +0200

    finailze wishes etc

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,14 +1,9 @@
 class Wish < ActiveRecord::Base
 
-  ACTIVITY_WISH     = 'WISH'
-  ACTIVITY_PROPOSAL = 'PROPOSAL'
-
-  ACTIVITIES = [ACTIVITY_WISH, ACTIVITY_PROPOSAL]
-
   belongs_to :user
   has_many :votes, :dependent => :destroy
 
-  validates :name, :description, :user, :activity, :presence => true
+  validates :name, :description, :user, :presence => true
 
   def stars
     return 0 if votes.empty?
