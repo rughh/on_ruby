7dc911ba64cf2813f8d57b6222e55ecf99ba32fc
phoet <phoetmail@googlemail.com>
2011-07-29 21:27:50 +0200

    use guard, add user show and sitemap.xml

--- app/models/usergroup.rb
+++
@@ -1,2 +0,0 @@
-class Usergroup < ActiveRecord::Base
-end
--- spec/views/home/index.html.haml_spec.rb
+++ spec/views/home/index.html.haml_spec.rb
@@ -1,17 +1,15 @@
 require "spec_helper"
 
-describe "/home/index" do
-  
-  before do
-    @event    = Factory(:event)
-    @user     = Factory(:user)
-    @location = Factory(:location)
-    @wish     = Factory(:wish)
-    @company  = Factory(:location, company: true)
-  end
+describe "home/index.html.haml" do
+
+  let(:event)     { Factory(:event) }
+  let(:user)      { Factory(:user) }
+  let(:location)  { Factory(:location) }
+  let(:wish)      { Factory(:wish) }
+  let(:company)   { Factory(:location, company: true) }
 
   it "should render successfully" do
-    view.stubs(events: [@event], current_event: @event, people: [@user], locations: [@location], wishes: [@wish], organizers: [@user], companies: [@company], main_user: @user)
+    view.stubs(events: [event], current_event: event, people: [user], locations: [location], wishes: [wish], organizers: [user], companies: [company], main_user: user)
     view.stubs(signed_in?: false)
     render
   end
