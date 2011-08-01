4323c0afbee2f8cd39e9a1afe6ab8ce647007729
phoet <phoetmail@googlemail.com>
2011-08-01 11:20:28 +0200

    fix next_event

--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -4,6 +4,7 @@ describe HomeHelper do
   describe "#next_event_date" do
     it "should always be on a wendsday" do
       next_event_date.should be_wednesday
+      next_event_date.should be > Date.today
     end
   end
 end
