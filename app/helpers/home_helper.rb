8a5b2f159d2ba69949e7437eaa1e780b404eb623
phoet <phoetmail@googlemail.com>
2011-08-01 11:20:28 +0200

    fix next_event

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,12 @@
 module HomeHelper
   def next_event_date
-    d = Date.today.at_beginning_of_month.next_month
-    d = d.wday > 3 ? d + (17 - d.wday).days : d + (10 - d.wday)
+    d = second_wednesday(Date.today)
+    d = second_wednesday(Date.today.next_month) if d < Date.today
     Time.utc(d.year, d.month, d.day, 19, 0)
+  end
+  
+  def second_wednesday(date)
+    d = date.at_beginning_of_month
+    d.wday > 3 ? d + (17 - d.wday).days : d + (10 - d.wday)
   end
 end
