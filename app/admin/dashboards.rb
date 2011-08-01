8952cfb457778600d5c10cd69d629b9f1ec5b22c
phoet <phoetmail@googlemail.com>
2011-08-01 11:54:01 +0200

    fix admin dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -2,7 +2,7 @@ ActiveAdmin::Dashboards.build do
 
   section "Recent Events" do
     ul do
-      Event.preview_events.collect do |event|
+      Event.latest.collect do |event|
         li link_to(event.name, admin_event_path(event))
       end
     end
