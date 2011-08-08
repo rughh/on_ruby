a7746eb03cd5898f647481d91eb5cfcc16d9eb7f
phoet <phoetmail@googlemail.com>
2011-08-08 13:29:38 +0200

    add automatic geocoding

--- app/admin/locations.rb
+++ app/admin/locations.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Location do
-  
+  form do |f|
+     f.inputs "Details" do
+       f.input :name
+       f.input :url
+       f.input :zip
+       f.input :city
+       f.input :street
+       f.input :house_number
+     end
+     f.buttons
+   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,15 +1,23 @@
 class Location < ActiveRecord::Base
+  
+  geocoded_by :full_address, latitude: :lat, longitude: :long
+  after_validation :geocode
 
-  belongs_to :event
+  has_many :events
 
-  validates :name, :url, :city, :street, :house_number, :zip, :lat, :long, :presence => true
+  validates :name, :url, :city, :street, :house_number, :zip, :presence => true
 
   scope :company, where(company: true)
+  scope :cometogether, joins(:event)
   
-  def self.cometogether
-    # hm, joins is not working on Location, produces strange sql:
-    # Location Load (0.2ms)  SELECT "locations".* FROM "locations" INNER JOIN "events" ON "events"."id" IS NULL
-    Event.joins(:location).includes(:location).map(&:location)
+  def full_address
+    "#{street} #{house_number}, #{zip} #{city}, Deutschland"
   end
+  
+  # def self.cometogether
+  #   # hm, joins is not working on Location, produces strange sql:
+  #   # Location Load (0.2ms)  SELECT "locations".* FROM "locations" INNER JOIN "events" ON "events"."id" IS NULL
+  #   Event.joins(:location).includes(:location).map(&:location)
+  # end
 
 end
