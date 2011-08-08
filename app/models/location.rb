47a7df6183dbbf16fde168d7a17027045c22bcbd
phoet <phoetmail@googlemail.com>
2011-08-08 13:57:05 +0200

    fix scope and some more user-details

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
-  
+
   geocoded_by :full_address, latitude: :lat, longitude: :long
   after_validation :geocode
 
@@ -8,16 +8,10 @@ class Location < ActiveRecord::Base
   validates :name, :url, :city, :street, :house_number, :zip, :presence => true
 
   scope :company, where(company: true)
-  scope :cometogether, joins(:event)
-  
+  scope :cometogether, joins(:events)
+
   def full_address
     "#{street} #{house_number}, #{zip} #{city}, Deutschland"
   end
-  
-  # def self.cometogether
-  #   # hm, joins is not working on Location, produces strange sql:
-  #   # Location Load (0.2ms)  SELECT "locations".* FROM "locations" INNER JOIN "events" ON "events"."id" IS NULL
-  #   Event.joins(:location).includes(:location).map(&:location)
-  # end
 
 end
