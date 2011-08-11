1e2c5ef0051410b6f3467c8d7aa00f77f94f8dc1
phoet <phoetmail@googlemail.com>
2011-08-11 21:04:31 +0200

    wording

--- spec/models/wish_spec.rb
+++ spec/models/wish_spec.rb
@@ -8,7 +8,7 @@ describe Wish do
   it "should generate a nice twitter message" do
     message = wish.twitter_message('http://bitly.url')
     message.length.should be < 140
-    message.should match "Neuer Wunsch von @uschi"
+    message.should match "Neues Thema von @uschi"
     message.should match "'The xing mobile website: touch.xing.com' http://bitly.url"
   end
 
