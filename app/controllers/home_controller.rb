e1bc44bdfc071c6260c13ae7ceb4da3bd89c35a6
phoet <phoetmail@googlemail.com>
2011-08-12 16:23:54 +0200

    add overviews for locations and companies

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   expose(:current_event) { Event.current.first }
   expose(:events) { Event.latest }
   expose(:people) { User.scoped }
-  expose(:main_user) { User.main }
   expose(:wishes) { Wish.latest }
   expose(:organizers) { User.organizer }
   expose(:locations) { Location.cometogether }
