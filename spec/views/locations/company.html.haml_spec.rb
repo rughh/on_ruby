3539d7017a51c40bc1fc5fbd23bf6283951685e7
phoet <phoetmail@googlemail.com>
2011-08-12 16:23:54 +0200

    add overviews for locations and companies

---
+++ spec/views/locations/company.html.haml_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe "locations/company.html.haml" do
+
+  let(:companies) { [Factory(:location, company: true)] }
+  let(:user) { Factory(:user) }
+
+  it "should render successfully" do
+    view.stubs(main_user: user, companies: companies, will_paginate: '')
+    render
+  end
+
+end
