d2230664e73eaa105c7d8a0be440d5df60d4f88e
phoet <phoetmail@googlemail.com>
2011-09-05 17:42:40 +0200

    remove recent events

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,13 +1,5 @@
 ActiveAdmin::Dashboards.build do
 
-  section "Recent Events" do
-    ul do
-      Event.latest.collect do |event|
-        li link_to(event.name, admin_event_path(event))
-      end
-    end
-  end
-
   section "Unpublished Events" do
     ul do
       Event.unpublished.collect do |event|
