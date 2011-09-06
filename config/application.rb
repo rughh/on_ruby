daee196d8c2acb7d92491a576ac5a0ed1aef05fe
rubiii <me@rubiii.com>
2011-09-06 18:56:41 +0200

    first step of migrating to rails 3.1
    todo:
    
    * fix the last two failing specs
    * fix the javascript
    * test activeadmin
    * move activeadmin css/js/images to vendor/assets
    * and a lot more testing

--- config/application.rb
+++ config/application.rb
@@ -2,21 +2,15 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require *Rails.groups(:assets => %w(development test))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module HamburgOnRuby
   class Application < Rails::Application
-
-    config.generators do |g|
-      g.template_engine :haml
-      g.test_framework  :rspec, :fixture => true, :views => false
-      g.integration_tool :rspec, :fixture => true, :views => true
-      g.fixture_replacement :factory_girl, :dir => "spec/support/factories"
-      g.stylesheets false
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -33,20 +27,33 @@ module HamburgOnRuby
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Berlin'
+    config.time_zone = "Berlin"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :de
-
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets.
+    config.assets.version = '1.0'
+
+    # Configure generators.
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :rspec, :fixture => true, :views => false
+      g.integration_tool :rspec, :fixture => true, :views => true
+      g.fixture_replacement :factory_girl, :dir => "spec/support/factories"
+#      g.stylesheets false
+      g.stylesheet_engine = :scss
+    end
 
   end
 end
--- config/boot.rb
+++ config/boot.rb
@@ -1,15 +1,8 @@
 require 'rubygems'
 require 'yaml'
-YAML::ENGINE.yamler= 'syck'
+YAML::ENGINE.yamler = 'syck'
 
 # Set up gems listed in the Gemfile.
-gemfile = File.expand_path('../../Gemfile', __FILE__)
-begin
-  ENV['BUNDLE_GEMFILE'] = gemfile
-  require 'bundler'
-  Bundler.setup
-rescue Bundler::GemNotFound => e
-  STDERR.puts e.message
-  STDERR.puts "Try running `bundle install`."
-  exit!
-end if File.exist?(gemfile)
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
