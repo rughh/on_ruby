cad395e5e0a48ef7f9fc0a2f2a9859ce4e7af7ef
phoet <phoetmail@googlemail.com>
2011-09-07 17:11:02 +0200

    cleanup mailer

--- spec/mailers/usergroup_mailer_spec.rb
+++ spec/mailers/usergroup_mailer_spec.rb
@@ -1,18 +1,17 @@
 require "spec_helper"
 
 describe UsergroupMailer do
-  before do
-    @event = Factory(:event)
-  end
+  
+  let(:event) { Factory(:event) }
 
   context "sending a mail" do
     it "should send the mail" do
-      email = UsergroupMailer.invitation_mail(@event).deliver
-      assert !ActionMailer::Base.deliveries.empty?
+      email = UsergroupMailer.invitation_mail(event).deliver
+      ActionMailer::Base.deliveries.should_not be_empty
       
       email.to.should eql([AppConfig.usergroup_email])
-      email.subject.should match(@event.name)
-      email.encoded.should =~ /<h1>#{@event.name}<\/h1>/
+      email.subject.should match(event.name)
+      email.encoded.should =~ /<h1>#{event.name}<\/h1>/
     end
   end
 end
