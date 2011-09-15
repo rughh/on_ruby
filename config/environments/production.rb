e1ee20240b9fcc3a228a9f1a91ebcc081dc230e2
phoet <phoetmail@googlemail.com>
2011-09-15 15:12:02 +0200

    add missing assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ HamburgOnRuby::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( active_admin.js )
+  config.assets.precompile += %w( active_admin.js active_admin.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
