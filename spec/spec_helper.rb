b7925ce30d69d8d2f459ed94883e4a69eec5b5ff
phoet <phoetmail@googlemail.com>
2011-09-29 14:01:54 +0200

    revert spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,56 +1,42 @@
-require 'spork'
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'capybara/rspec'
 
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+Capybara.javascript_driver = :selenium
 
-Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However, 
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
-  require 'capybara/rspec'
+RSpec.configure do |config|
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  # config.mock_with :rspec
 
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  Capybara.javascript_driver = :selenium
-
-  RSpec.configure do |config|
-    # == Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
-    # config.mock_with :rspec
-
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    # instead of true.
-    config.use_transactional_fixtures = true
-    
-    # register global before stuff TODO (ps) why this no work?!
-    # config.before(:type => :controller) do
-    #   request.env["HTTP_REFERER"] = "/"
-    # end
-
-  end
-
-  module SpecHelper
-    def admin_user
-      @admin_user ||= Factory(:user, :admin => true)
-    end
-  end
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+  
+  # register global before stuff TODO (ps) why this no work?!
+  # config.before(:type => :controller) do
+  #   request.env["HTTP_REFERER"] = "/"
+  # end
 
 end
 
-Spork.each_run do
-  load "#{Rails.root}/config/routes.rb"
-  Dir["#{Rails.root}/app/**/*.rb"].each { |f| load f }
+module SpecHelper
+  def admin_user
+    @admin_user ||= Factory(:user, :admin => true)
+  end
 end
