6b01f5b61fe80634455f5f3fa53c8ace77aaea12
phoet <phoetmail@googlemail.com>
2011-10-06 16:38:01 +0200

    remove remarkabel gem

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe Participant do
 
-  it { should validate_presence_of(:user, :event) }
+  let(:participant) { Participant.new }
+
+  it "should validate presence" do
+    participant.should have(1).errors_on(:event)
+    participant.should have(1).errors_on(:user)
+  end
 
 end
