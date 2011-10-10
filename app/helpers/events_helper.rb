f3c18ed3e2707eef3dbf3a6adfc7ff6cdba575a7
phoet <phoetmail@googlemail.com>
2011-10-10 21:27:53 +0200

    add fancy-box stuff

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,8 +1,9 @@
 module EventsHelper
   def event_with_tooltip(event)
-    content_tag(:p, event.description.truncate(100)) +
+    content_tag(:h2, event.name) +
+    content_tag(:p, event.description) +
     content_tag(:ul, '', class: 'enumeration') do
-      event.topics.first(3).map do |topic|
+      event.topics.map do |topic|
         content_tag :li, topic.name
       end.join.html_safe
     end
