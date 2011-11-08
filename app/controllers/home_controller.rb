5a47a4f9a5019fcbfcc6909cd2b4d8dc80b79eaa
Peter Schröder <phoetmail@googlemail.com>
2011-11-08 21:00:20 +0100

    add js for extending lists and fix spec

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   expose(:current_event) { Event.current.first }
   expose(:events) { Event.latest }
   expose(:people) { User.scoped }
-  expose(:wishes) { Wish.latest }
+  expose(:wishes_undone) { Wish.undone }
+  expose(:wishes_done) { Wish.done }
   expose(:organizers) { User.organizer }
   expose(:locations) { Location.cometogether }
   expose(:companies) { Location.company }
