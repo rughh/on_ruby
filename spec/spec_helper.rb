82f0c50440f5ce48ca7103d9b7e7089d2a7d8c7b
Peter Schröder <phoetmail@googlemail.com>
2011-11-28 16:09:51 +0100

    use rails 3.1.3, update gems, remove acceptance spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,10 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
-Capybara.javascript_driver = :selenium
 
 RSpec.configure do |config|
   # == Mock Framework
