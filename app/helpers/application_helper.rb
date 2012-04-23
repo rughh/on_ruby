c84c316b2d1d476aca16c4072b1742fd265df6d5
Peter Schröder <phoetmail@googlemail.com>
2012-04-23 16:05:34 +0200

    fix broken img for github ribbons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,7 +55,7 @@ module ApplicationHelper
 
   def fork_me_ribbon
     target = "https://github.com/phoet/hamburg_on_ruby"
-    img = "https://a248.e.akamai.net/assets.github.com/img/4c7dc970b89fd04b81c8e221ba88ff99a06c6b61/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67"
+    img    = "https://a248.e.akamai.net/camo.github.com/4c7dc970b89fd04b81c8e221ba88ff99a06c6b61/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67"
     link_to target do
       image_tag img, alt: "Fork me on GitHub", style: "position: absolute; top: 0; right: 0; border: 0; margin-top: 40px"
     end
