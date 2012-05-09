6923d2beb17fbe8c7dba0aa7b9eb335742078560
rubiii <me@rubiii.com>
2012-05-09 11:00:38 +0200

    explicit error for missing $SECRET_TOKEN

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HamburgOnRuby::Application.config.secret_token = ENV['SECRET_TOKEN']
+if ENV['SECRET_TOKEN']
+  HamburgOnRuby::Application.config.secret_token = ENV['SECRET_TOKEN']
+else
+  fail "Missing env variable $SECRET_TOKEN (to specify the app's secret token)"
+end
