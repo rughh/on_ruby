b8ee5867f5a1a508176401cc1b0cf6f8c5d006fd
Peter Schröder <phoetmail@googlemail.com>
2012-06-05 16:11:58 +0200

    disable data migration in migrations

--- db/migrate/20120128121241_add_slugs.rb
+++ db/migrate/20120128121241_add_slugs.rb
@@ -3,10 +3,10 @@ class AddSlugs < ActiveRecord::Migration
     [:users, :events, :wishes].each do |table|
       add_column table, :slug, :string
       add_index  table, :slug, :unique => true
-      "#{table}".singularize.capitalize.constantize.all.each do |model|
-        puts "updating #{model.id}"
-        model.save!
-      end
+      # "#{table}".singularize.capitalize.constantize.all.each do |model|
+      #   puts "updating #{model.id}"
+      #   model.save!
+      # end
     end
   end
 
