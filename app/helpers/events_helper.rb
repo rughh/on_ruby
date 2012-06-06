52dbe9dc2b503179d6d786bfbfb6bdafdf41f291
Peter Schröder <phoetmail@googlemail.com>
2012-06-06 20:07:02 +0200

    cleanup the events page and fix some massasignment stuff

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,4 +1,14 @@
 module EventsHelper
+  def participation_link
+    if signed_in?
+      if current_user.participates?(event)
+        button_to t("show.not_attend"), event_participant_path(event, current_user.participation(event)), method: 'delete'
+      else
+        button_to t("show.attend"), add_user_event_path(event)
+      end
+    end
+  end
+
   def event_with_tooltip(event)
     content_tag(:h2, event.name) +
     content_tag(:p, event.description) +
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,7 @@
 class Job < ActiveRecord::Base
   validates :name, :url, :location, presence: true
 
-  attr_accessible :name, :url, :location, as: :admin
+  attr_accessible :name, :url, :location, :location_id, as: :admin
 
   default_scope -> { where(:label => Whitelabel[:label_id]) }
 
