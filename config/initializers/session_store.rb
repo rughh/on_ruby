a515df47b5bfc80f1987d70cb886cb2c92997bce
Peter Schröder <phoetmail@googlemail.com>
2012-06-06 19:38:46 +0200

    change cookie domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # TODO: this probably causes quite some trouble, but according to what i've read,
 # you need to specifiy the domain to allow cookies to be shared between subdomains.
 # http://stackoverflow.com/a/10403338/279024 [dh, 2012-05-13]
-domain = Rails.env.production? ? "onruby.de" : "local.de"
+domain = Rails.env.production? ? "onruby.de" : "onruby.dev"
 HamburgOnRuby::Application.config.session_store :cookie_store, key: "_on_ruby_session", domain: domain
 
 
