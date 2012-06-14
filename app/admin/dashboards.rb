479b72df3701555f393bf7740512590cfb194b1a
Peter Schröder <phoetmail@googlemail.com>
2012-06-14 14:44:27 +0200

    move view logic from views to helpers

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -3,7 +3,7 @@ ActiveAdmin::Dashboards.build do
     div link_to 'Duplicate latest Event', duplicate_admin_events_path
     ul do
       Event.unpublished.collect do |event|
-        item = event.name + ": " + link_to("tweet", twitter_helper.twitter_update_url(event.twitter_message(event_url(event)))) + " / " + link_to("publish", publish_admin_event_path(event))
+        item = event.name + ": " + link_to("tweet", twitter_update_url(event)) + " / " + link_to("publish", publish_admin_event_path(event))
         li raw(item)
       end
     end
@@ -12,7 +12,7 @@ ActiveAdmin::Dashboards.build do
   section "Open Wishes" do
     ul do
       Wish.undone.collect do |wish|
-        item = wish.name + ": " + link_to("tweet", twitter_helper.twitter_update_url(wish.twitter_message(wish_url(wish)))) + " / " + link_to("for next Event", copy_admin_wish_path(wish))
+        item = wish.name + ": " + link_to("tweet", twitter_update_url(wish)) + " / " + link_to("for next Event", copy_admin_wish_path(wish))
         li raw(item)
       end
     end
@@ -25,10 +25,4 @@ ActiveAdmin::Dashboards.build do
       end
     end
   end
-end
-
-def twitter_helper
-  helper = Object.new
-  helper.send :extend, ApplicationHelper
-  helper
 end
