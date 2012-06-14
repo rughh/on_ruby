a592e2ef4736879937cc31d2f3b6bff73d13d33f
Peter Schröder <phoetmail@googlemail.com>
2012-06-14 15:17:57 +0200

    add an (awkward) possibility for custom "next event" option

--- config/initializers/whitelabel.rb
+++ config/initializers/whitelabel.rb
@@ -1,3 +1,44 @@
 Usergroup = Struct.new :label_id, :email, :host, :twitter, :usergroup_email, :organizers, :location, :imprint, :other_usergroups
 
+# TODO (ps) this inheritance cold be greatly reduced with some configuration option:
+# ie: recurring: every_third_wednesday
+class Rughh < Usergroup
+  def next_event_date
+    d = second_wednesday(Date.today)
+    d = second_wednesday(Date.today.next_month) if d < Date.today
+    Time.new(d.year, d.month, d.day, 19, 0)
+  end
+
+  def second_wednesday(date)
+    d = date.at_beginning_of_month
+    d.wday > 3 ? d + (17 - d.wday).days : d + (10 - d.wday)
+  end
+end
+
+class Hackhb < Usergroup
+  def next_event_date
+    d = first_wednesday(Date.today)
+    d = first_wednesday(Date.today.next_month) if d < Date.today
+    Time.new(d.year, d.month, d.day, 19, 0)
+  end
+
+  def first_wednesday(date)
+    d = date.at_beginning_of_month
+    d.wday > 3 ? d + (10 - d.wday).days : d + (3 - d.wday)
+  end
+end
+
+class Colognerb < Usergroup
+  def next_event_date
+    d = third_wednesday(Date.today)
+    d = third_wednesday(Date.today.next_month) if d < Date.today
+    Time.new(d.year, d.month, d.day, 19, 0)
+  end
+
+  def third_wednesday(date)
+    d = date.at_beginning_of_month
+    d.wday > 3 ? d + (24 - d.wday).days : d + (17 - d.wday)
+  end
+end
+
 Whitelabel.from_file Rails.root.join("config/whitelabel.yml")
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -28,15 +28,4 @@ describe Event do
     10.times{|i| create(:event, :name => "Event #{i}", :date => (Time.now - i.weeks)) }
     Event.latest.map(&:name).should == ["Event 1", "Event 2", "Event 3", "Event 4", "Event 5", "Event 6", "Event 7", "Event 8", "Event 9"]
   end
-
-  describe "#next_event_date" do
-    it "should always be in the future on wednesday" do
-      Event.next_event_date.should be_wednesday
-      Event.next_event_date.should be > Date.today
-    end
-
-    it "should find the second wednesday" do
-      Event.second_wednesday(Time.new(2011, 9, 1, 0, 0, 0)).should eql(Time.new(2011, 9, 14, 0, 0, 0))
-    end
-  end
 end
