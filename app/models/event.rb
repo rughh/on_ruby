c8beab4c3ae98bee8d29b9c10f305b230132f5e4
Peter Schröder <phoetmail@googlemail.com>
2012-06-14 15:17:57 +0200

    add an (awkward) possibility for custom "next event" option

--- app/models/event.rb
+++ app/models/event.rb
@@ -72,16 +72,8 @@ class Event < ActiveRecord::Base
   end
 
   class << self
-    # FIXME (ps) this needs to be configurable (somehow...)
     def next_event_date
-      d = second_wednesday(Date.today)
-      d = second_wednesday(Date.today.next_month) if d < Date.today
-      Time.new(d.year, d.month, d.day, 19, 0)
-    end
-
-    def second_wednesday(date)
-      d = date.at_beginning_of_month
-      d.wday > 3 ? d + (17 - d.wday).days : d + (10 - d.wday)
+      Whitelabel.label.next_event_date
     end
 
     def duplicate!
@@ -93,5 +85,4 @@ class Event < ActiveRecord::Base
       end
     end
   end
-
 end
