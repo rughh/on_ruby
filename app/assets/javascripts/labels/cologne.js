c4eb81e0c4fad4b73f45dc3c0948aaf59f15baba
Jakob Hilden <jakobhilden@gmail.com>
2012-06-21 09:01:38 +0200

    fixed title animation for colognerb.  had to change a little bit of the markup on the way

--- app/assets/javascripts/labels/cologne.js
+++ app/assets/javascripts/labels/cologne.js
@@ -1,17 +1,24 @@
 var CGN = {
   ratio: 0,
+  // header animation
   showKoelsch: function() {
-    $("header h1 a").text("Kölsch.rb");
-    $("header h2 a").text("De Kölsche Ruby Jecke");
+    $("header h1").text("Kölsch.rb");
+    $("header h2").text("De Kölsche Ruby Jecke");
   },
   showCologne: function() {
-    $("header h1 a").text("Cologne.rb");
-    $("header h2 a").text("Ruby User Group Cologne");
+    $("header h1").text("Cologne.rb");
+    $("header h2").text("Ruby User Group Cologne");
   }
 };
+
 $(document).ready(function() {
-  $("header h1, header h2").mouseover(CGN.showKoelsch).mouseout(CGN.showCologne);
+  // assign header animation listeners
+  $("#title").mouseover(CGN.showKoelsch).mouseout(CGN.showCologne);
+
+  // append custom markup for gem beer glas
   $(".main").append("<div id='gem'><img src='/assets/labels/cologne/gem-glas.png'><div id='beer'></div></div><span><a href='#on_ruby' id='refill'>refill?</a></span>");
+
+  // animate gem beer glas
   CGN.beer_height = $("#gem").height();
   $(window).scroll(function() {
     CGN.ratio = $(window).scrollTop() / ($(document).height() - $(window).height());
