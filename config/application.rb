eeabba8cef69e17789b78fa3d92251932c0fb01c
Peter Schröder <phoetmail@googlemail.com>
2012-08-31 21:11:19 +0200

    use slim instead of haml, closes #26

--- config/application.rb
+++ config/application.rb
@@ -45,11 +45,11 @@ module OnRuby
 
     # Configure generators.
     config.generators do |g|
-      g.template_engine :haml
+      g.template_engine :slim
       g.test_framework :rspec, :fixture => true, :views => false
       g.integration_tool :rspec, :fixture => true, :views => true
       g.fixture_replacement :factory_girl, :dir => "spec/support/factories"
-      g.stylesheet_engine = :scss
+      g.stylesheet_engine = :sass
     end
 
     # Mass assignment settings
