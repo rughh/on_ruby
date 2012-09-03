2f4efd15cc0e4062f647f4e0a732a349bfeb88c4
Peter Schröder <phoetmail@googlemail.com>
2012-09-03 20:57:03 +0200

    fix the sitemap.xml for all the mandants

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,12 +1,25 @@
 class MiscController < ApplicationController
-  def sitemap
-    @urls = []
-    @urls << root_url
-    @urls += Wish.all.map {|wish| url_for wish}
-    @urls += Event.all.map {|event| url_for event}
-    @urls += User.all.map {|user| url_for user}
+  skip_before_filter :switch_label, only: :sitemap
+  skip_before_filter :switch_locale, only: :sitemap
 
+  helper_method :urls
+
+  def sitemap
     response.headers["Content-Type"] = 'text/xml'
     render layout: false
+  end
+
+  private
+
+  def urls
+    Whitelabel.labels.map do |label|
+      Whitelabel.with_label(label) do
+        subdomain = label.label_id
+        wishes = Wish.all.map {|wish| wish_url wish, subdomain: subdomain}
+        users  = User.all.map {|user| user_url user, subdomain: subdomain}
+        events = Event.all.map {|event| event_url event, subdomain: subdomain}
+        [root_url(subdomain: subdomain)] + wishes + events + users
+      end
+    end.flatten
   end
 end
--- spec/controllers/misc_controller_spec.rb
+++ spec/controllers/misc_controller_spec.rb
@@ -1,15 +1,18 @@
 require 'spec_helper'
 
 describe MiscController do
-
-  before { set_subdomain }
-
   describe "GET :sitemap" do
-    before { get :sitemap, format: :xml }
+    render_views
+
+    before(:each) do
+      create(:user)
+    end
 
     it "should render the :index template" do
+      get :sitemap, format: :xml
       response.should render_template(:sitemap)
+      response.body.should match("<loc>http://hamburg.test.host/")
+      response.body.should match("<loc>http://hamburg.test.host/users/uschi-")
     end
   end
-
 end
