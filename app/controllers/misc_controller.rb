f76114e86901475d4d7b0fe6ec3c7a9e9cabcdc2
Peter Schröder <phoetmail@googlemail.com>
2012-09-19 15:18:34 +0200

    add locations to sitemap

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -14,10 +14,11 @@ class MiscController < ApplicationController
     Whitelabel.labels.map do |label|
       Whitelabel.with_label(label) do
         subdomain = label.label_id
-        wishes = Wish.all.map {|wish| wish_url wish, subdomain: subdomain}
-        users  = User.all.map {|user| user_url user, subdomain: subdomain}
-        events = Event.all.map {|event| event_url event, subdomain: subdomain}
-        [root_url(subdomain: subdomain)] + wishes + events + users
+        wishes    = Wish.all.map { |wish| wish_url wish, subdomain: subdomain }
+        users     = User.all.map { |user| user_url user, subdomain: subdomain }
+        events    = Event.all.map { |event| event_url event, subdomain: subdomain }
+        locations = Location.all.map { |location| location_url location, subdomain: subdomain }
+        [root_url(subdomain: subdomain)] + wishes + events + users + locations
       end
     end.flatten
   end
