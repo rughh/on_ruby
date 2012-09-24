b3aa11aa4303bba715a7222fd66db1cea2880466
Peter Schröder <phoetmail@googlemail.com>
2012-09-24 21:46:06 +0200

    add mobile static maps

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,3 +1,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.mobile
+
+$(document).bind('pageinit', function() {
+  var map = $("#mobile_map");
+  if (map) {
+    var screenWidth = $(window).width();
+    var relativeWidth = screenWidth - (screenWidth * 0.1);
+    var url = map.data('url') + '&size=' + relativeWidth + 'x' + relativeWidth;
+    map.html('<img src="' +  url + '" />');
+  }
+});
