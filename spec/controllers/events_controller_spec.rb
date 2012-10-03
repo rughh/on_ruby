90e2d1ff73caade4bb2fb08729965dfa60ac90c1
Peter Schröder <phoetmail@googlemail.com>
2012-10-03 12:16:28 +0200

    accepting participations via json

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -35,24 +35,39 @@ describe EventsController do
   end
 
   describe "POST :add_user" do
-    it "should add a prticipant for current user" do
+    before do
       controller.stubs(current_user: user)
+    end
 
+    it "should add a user via json post" do
+      expect { post :add_user, format: :json, id: event.id }.to change(Participant, :count).by(1)
+      response.status.should be(201)
+    end
+
+    it "should add a prticipant for current user" do
       expect { post :add_user, id: event.id }.to change(Participant, :count).by(1)
 
       flash[:notice].should_not be_nil
       response.should redirect_to(event)
     end
 
-    it "should should not add paritcipant if already exists" do
-      user.participants.create(event: event)
-      controller.stubs(current_user: user)
+    context "with an already participating user" do
+      before do
+        user.participants.create(event: event)
+      end
 
-      expect { post :add_user, id: event.id }.to change(Participant, :count).by(0)
+      it "should silently ignore the problem via json" do
+        expect { post :add_user, format: :json, id: event.id }.to change(Participant, :count).by(0)
 
-      flash[:alert].should_not be_nil
-      response.should redirect_to(event)
+        response.status.should be(200)
+      end
+
+      it "should should alert a duplicate flash" do
+        expect { post :add_user, id: event.id }.to change(Participant, :count).by(0)
+
+        flash[:alert].should_not be_nil
+        response.should redirect_to(event)
+      end
     end
   end
-
 end
