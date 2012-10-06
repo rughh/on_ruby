944219a3aefd3c2f1bda2e6feb5b863ef5511f5e
Peter Schröder <phoetmail@googlemail.com>
2012-10-06 20:38:40 +0200

    cleanup the spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,34 +1,16 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"]    ||= 'test'
 ENV["SECRET_TOKEN"] ||= 'SECRET_TOKEN_TEST_b7c7374eb0285b87c0c1c61c2c5401b9f92dd59209713801743202c'
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
   config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  # config.mock_with :rspec
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = true
-
   config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.filter_run :focus => true
+  config.filter_run focus: true
   config.run_all_when_everything_filtered = true
 
   config.before do
@@ -36,6 +18,6 @@ RSpec.configure do |config|
     Whitelabel.label = Whitelabel.labels.first
   end
 
-  config.include RequestHelper, :type => :controller
+  config.include RequestHelper, type: :controller
   config.include FactoryGirl::Syntax::Methods
 end
