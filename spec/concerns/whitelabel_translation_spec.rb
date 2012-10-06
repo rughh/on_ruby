37b2f9129cecb3faed4a6e1a3d6bebf5e5d273ca
Peter Schröder <phoetmail@googlemail.com>
2012-10-06 20:31:19 +0200

    change describe

--- spec/concerns/whitelabel_translation_spec.rb
+++ spec/concerns/whitelabel_translation_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe I18n do
+describe WhitelabelTranslation do
   describe "#tw" do
     it "should translate in scope of a whitelabel" do
       I18n.tw("title").should eql("Hamburg on Ruby")
