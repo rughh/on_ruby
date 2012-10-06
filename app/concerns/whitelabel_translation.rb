724fbacb51ace324e16f6ea0d5316c18fda9d474
Peter Schröder <phoetmail@googlemail.com>
2012-10-06 20:28:55 +0200

    move the translation stuff to a concern

--- app/concerns/whitelabel_translation.rb
+++ app/concerns/whitelabel_translation.rb
@@ -1,9 +1,17 @@
-module I18n
-  class << self
+require 'active_support/concern'
+
+module WhitelabelTranslation
+  extend ActiveSupport::Concern
+
+  module ClassMethods
     def translate_whitelabel(token, options = {})
       sub = Whitelabel.label ? Whitelabel[:label_id] : "default"
       t(token, options.merge(:scope => "label.#{sub}"))
     end
     alias :tw :translate_whitelabel
   end
+end
+
+module I18n
+  include WhitelabelTranslation
 end
