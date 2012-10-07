8cf5a36a34ce6acc08bdeebfd5d73c8388daea57
Peter Schröder <phoetmail@googlemail.com>
2012-10-07 14:30:12 +0200

    cleanup the auth stuff

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,5 +1,4 @@
 class Authorization < ActiveRecord::Base
-
   belongs_to :user
 
   validates :user_id, :uid, :provider, presence: true
@@ -7,13 +6,17 @@ class Authorization < ActiveRecord::Base
 
   attr_accessible :user, :uid, :provider
 
-  def self.find_from_hash(hash)
-    find_by_provider_and_uid(hash['provider'], hash['uid'])
-  end
+  def self.handle_authorization auth
+    provider = auth['provider']
+    uid      = auth['uid']
 
-  def self.create_from_hash(hash, user = nil)
-    user ||= User.create_from_hash!(hash)
-    Authorization.create! user: user, uid: hash['uid'], provider: hash['provider']
+    authorization = find_by_provider_and_uid provider, uid
+    if authorization.present?
+      authorization.user.update_from_auth! auth
+    else
+      user = User.create_from_hash! auth
+      authorization = Authorization.create! user: user, provider: provider, uid: uid
+    end
+    authorization
   end
-
 end
