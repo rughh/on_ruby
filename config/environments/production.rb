2799a6a85b76a63069e8634b4d91df19f42e370e
Peter Schröder <phoetmail@googlemail.com>
2012-10-08 16:42:31 +0200

    try to get this damn mailer working again for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,13 +61,14 @@ OnRuby::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
-    :port           => "25",
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV['SENDGRID_DOMAIN']
+    :domain         => 'heroku.com'
   }
+  config.action_mailer.delivery_method = :smtp
 
   # Enable threaded mode
   # config.threadsafe!
