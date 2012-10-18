b012d546c68bc731c32f634f64869ef9283cbcfc
Peter Schröder <phoetmail@googlemail.com>
2012-10-18 08:46:01 +0200

    adapt latest to current date logic

--- app/models/event.rb
+++ app/models/event.rb
@@ -35,7 +35,7 @@ class Event < ActiveRecord::Base
   default_scope -> { where(:label => Whitelabel[:label_id]) }
 
   scope :current, lambda{ where(date: Date.today.to_time..(Time.now + 8.weeks)).limit(1).order('date ASC') }
-  scope :latest, where('date < ?', Time.now).order('date DESC')
+  scope :latest, where('date < ?', Date.today.to_time).order('date DESC')
   scope :unpublished, where(published: false)
   scope :ordered, order("date DESC")
 
