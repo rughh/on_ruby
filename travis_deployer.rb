a536e1acf76041eaf545096210b690001b1c9721
Peter Schröder <phoetmail@googlemail.com>
2012-10-22 12:06:52 +0200

    try to deploy directly from travis

---
+++ travis_deployer.rb
@@ -0,0 +1,17 @@
+File.open(".git/config", "a") do |f|
+  f.puts <<-EOF
+[remote "heroku"]
+    url = url = git@heroku.com:your-app-here.git
+    fetch = +refs/heads/*:refs/remotes/heroku/* # had to change this into remote name instead of app name
+EOF
+end
+
+known_hosts = File.expand_path("~/.ssh/config")
+File.open(known_hosts, "a") do |f|
+  f.puts <<-EOF
+Host heroku.com
+   StrictHostKeyChecking no
+   CheckHostIP no
+   UserKnownHostsFile=/dev/null
+EOF
+end
