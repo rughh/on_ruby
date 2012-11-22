1605cac07501454112c45e5547ee26d4845a89b5
Peter Schröder <phoetmail@googlemail.com>
2012-11-22 16:20:17 +0100

    disable asset logging, cause rails is not able to do it

---
+++ app/middlewares/disable_assets_logger.rb
@@ -0,0 +1,15 @@
+# https://github.com/rails/rails/issues/2639#issuecomment-6591735
+class DisableAssetsLogger
+  def initialize(app)
+    @app = app
+    Rails.application.assets.logger = Logger.new('/dev/null')
+  end
+
+  def call(env)
+    previous_level = Rails.logger.level
+    Rails.logger.level = Logger::ERROR if env['PATH_INFO'].index("/assets/") == 0
+    @app.call(env)
+  ensure
+    Rails.logger.level = previous_level
+  end
+end
