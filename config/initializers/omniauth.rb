5fbab3fbb8fec0e308aff19880e6c1f3eeea5a3e
Peter Schröder <phoetmail@googlemail.com>
2012-12-15 16:41:41 +0100

    implement dynamic github auth providers, closes #57
    each custom domain can now be supported by it's own github app authentication.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,19 @@
+OmniAuth.config.logger = Rails.logger
+
+# production settings
+OMNIAUTH_GITHUB_KEYS = Hash.new([ENV['GITHUB_CONSUMER_KEY'], ENV['GITHUB_CONSUMER_SECRET']])
+OMNIAUTH_GITHUB_KEYS["rug-b.de"]           = [ENV['GITHUB_RUGB_CONSUMER_KEY'], ENV['GITHUB_RUGB_CONSUMER_SECRET']]
+# development settings
+OMNIAUTH_GITHUB_KEYS["hamburg.onruby.dev"] = [ENV['GITHUB_DEV_ONRUBY_CONSUMER_KEY'], ENV['GITHUB_DEV_ONRUBY_CONSUMER_SECRET']]
+OMNIAUTH_GITHUB_KEYS["rug-b.dev"]          = [ENV['GITHUB_DEV_RUGB_CONSUMER_KEY'], ENV['GITHUB_DEV_RUGB_CONSUMER_SECRET']]
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
-  provider :github,  ENV['GITHUB_CONSUMER_KEY'],  ENV['GITHUB_CONSUMER_SECRET']
+  provider :github, {
+    setup: Proc.new do |env|
+      client_id, client_secret = OMNIAUTH_GITHUB_KEYS[env['SERVER_NAME']]
+      env['omniauth.strategy'].options[:client_id]     = client_id
+      env['omniauth.strategy'].options[:client_secret] = client_secret
+    end
+  }
 end
