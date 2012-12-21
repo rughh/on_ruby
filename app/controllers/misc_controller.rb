44638f8090b66341b102a6890fc8ca94bc3049c5
Peter Schröder <phoetmail@googlemail.com>
2012-12-21 19:57:36 +0100

    render something

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -6,6 +6,7 @@ class MiscController < ApplicationController
     data = {:data => {:message => "was doing something wrong"}}
     error = ExceptionNotifier::Notifier.exception_notification(request.env, Exception.new, data)
     error.deliver
+    render text: "ERROR DELIVERED"
   end
 
   def sitemap; end
