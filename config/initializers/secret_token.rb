d1a4ca289091545d16e9ddb7e69593777f772b9b
Peter Schröder <phoetmail@googlemail.com>
2013-05-07 21:24:12 +0200

    add secret base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-OnRuby::Application.config.secret_token = ENV['SECRET_TOKEN']
+OnRuby::Application.config.secret_token     = ENV['SECRET_TOKEN']
+OnRuby::Application.config.secret_key_base  = ENV['SECRET_TOKEN'].reverse
