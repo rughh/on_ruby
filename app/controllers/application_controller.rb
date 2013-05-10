8c044e783a58ca35dcc3316f8510f158ac563412
Peter Schröder <phoetmail@googlemail.com>
2013-05-10 21:02:54 +0200

    group by type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
 
   before_filter :setup
 
-  helper_method :current_user
-  helper_method :signed_in?, :mobile_device?
+  helper_method :signed_in?, :current_user
+  helper_method :mobile_device?
 
   cache_sweeper :index_sweeper
 
