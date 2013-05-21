25c4fe37c659b4598cb3c72b5cb5b691833fe7d4
Peter Schröder <phoetmail@googlemail.com>
2013-05-21 16:46:28 +0200

    fix error when no user given for a material

--- spec/views/events/show.html_spec.rb
+++ spec/views/events/show.html_spec.rb
@@ -8,4 +8,12 @@ describe "events/show" do
     view.stubs(signed_in?: true, current_user: user, event: event)
     render
   end
+
+  context "materials" do
+    let(:materials) { [build(:material)] }
+
+    it "renders without an error" do
+      render partial: 'materials', locals: {materials: materials}
+    end
+  end
 end
