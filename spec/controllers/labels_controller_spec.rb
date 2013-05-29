a4971d8a5edde71da15c2b40f3300b9323642144
Peter Schröder <phoetmail@googlemail.com>
2013-05-29 16:11:53 +0200

    provide better spec for labels page

--- spec/controllers/labels_controller_spec.rb
+++ spec/controllers/labels_controller_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe LabelsController do
   context "GET :index" do
-    it "should render the :index template" do
+    it "renders the :index template" do
+      get :index
+      response.should render_template(:index)
+    end
+
+    it "renders the :index template for mobile devices" do
       get :index
       response.should render_template(:index)
     end
