a27053b8a38a2a7194bf0213a429e59edff7856e
Peter Schröder <phoetmail@googlemail.com>
2013-05-29 16:10:35 +0200

    split stuff and move it to request specs

--- spec/controllers/sitemaps_controller_spec.rb
+++ spec/controllers/sitemaps_controller_spec.rb
@@ -2,18 +2,10 @@ require 'spec_helper'
 
 describe SitemapsController do
   context "GET :show" do
-    render_views
-
-    before do
-      create(:participant_user)
-    end
-
     it "renders a sitemap" do
       get :show, id: 'hamburg', format: :xml
       response.should render_template(:show)
       response.headers["Content-Type"].should eql("application/xml; charset=utf-8")
-      response.body.should match("<loc>http://hamburg.test.host/")
-      response.body.should match("<loc>http://hamburg.test.host/users/uschi-")
     end
   end
 end
---
+++ spec/requests/sitemaps_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe SitemapsController do
+  context "GET :show" do
+    before do
+      create(:participant_user)
+    end
+
+    it "renders a sitemap" do
+      get sitemap_path(id: 'hamburg', format: :xml)
+      response.body.should match("<loc>http://hamburg.onruby.dev/")
+      response.body.should match("<loc>http://hamburg.onruby.dev/users/uschi-")
+    end
+  end
+end
