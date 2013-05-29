adc08b4cb6c551ab50da54e92073071ebabcd80c
Peter Schröder <phoetmail@googlemail.com>
2013-05-29 16:10:13 +0200

    remove some trash

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,16 +1,7 @@
 require 'spec_helper'
 
 describe "Events" do
-  before { ActionDispatch::Request.any_instance.stubs(:subdomains).returns(['hamburg']) }
-
   let(:event) { create(:event) }
-
-  context "GET /events" do
-    it "works!" do
-      get event_path(event)
-      response.status.should be(200)
-    end
-  end
 
   context "GET /events.son" do
     it "renders valid json" do
