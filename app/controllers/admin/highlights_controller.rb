a0443896efff3fb0eea03746064e4a13dd94a17e
Peter Schröder <phoetmail@googlemail.com>
2013-06-20 00:10:42 -0400

    cleanup the typus stuff and add back custom methods

--- app/controllers/admin/highlights_controller.rb
+++ app/controllers/admin/highlights_controller.rb
@@ -1,2 +1,7 @@
 class Admin::HighlightsController < Admin::BaseAdminController
+  def disable
+    highlight = Highlight.find(params[:id])
+    highlight.disable!
+    redirect_to url_for(controller: "/admin/highlights", action: :edit, id: highlight.id), notice: "Disabled!"
+  end
 end
