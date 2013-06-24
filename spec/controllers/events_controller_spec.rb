62adabba249a9d0c6b62819a03f9004b0573a7df
Peter Schröder <phoetmail@googlemail.com>
2013-06-23 22:30:50 -0400

    don't touch the db

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,31 +1,30 @@
 require 'spec_helper'
 
 describe EventsController do
-  let(:event) { create(:event) }
+  let(:event) { build(:event) }
 
   context "GET :index" do
-    before { get :index, format: :xml }
-
     it "should assign the events to display" do
+      get :index, format: :xml
       controller.events.should_not be_nil
     end
 
     it "should render the :index template" do
+      get :index, format: :xml
       response.headers["Content-Type"].should eql("application/xml; charset=utf-8")
       response.should render_template(:index)
     end
   end
 
   context "GET :show" do
-    before do
-      get :show, id: event.id
-    end
-
     it "should assign the requested event" do
+      controller.stubs(event: event)
+      get :show, id: event.id
       controller.event.should eql(event)
     end
 
     it "should render the :show template" do
+      get :show, id: event.id
       response.should render_template(:show)
     end
   end
