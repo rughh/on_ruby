a649460479fa9f3818a82643d8ce4923693f6969
Peter Schröder <phoetmail@googlemail.com>
2013-06-23 22:42:13 -0400

    improve test coverage on events controller

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -4,28 +4,53 @@ describe EventsController do
   let(:event) { build(:event) }
 
   context "GET :index" do
-    it "should assign the events to display" do
-      get :index, format: :xml
+    it "assign the events and renders the template" do
+      get :index
       controller.events.should_not be_nil
+      response.should render_template(:index)
     end
 
-    it "should render the :index template" do
-      get :index, format: :xml
-      response.headers["Content-Type"].should eql("application/xml; charset=utf-8")
-      response.should render_template(:index)
+    context "json" do
+      it "renders json" do
+        controller.stubs(events: [event])
+        get :index, format: :json
+        response.headers["Content-Type"].should eql("application/json; charset=utf-8")
+        JSON.parse(response.body).should have(1).elements
+      end
+    end
+
+    context "xml" do
+      it "renders xml" do
+        get :index, format: :xml
+        response.headers["Content-Type"].should eql("application/xml; charset=utf-8")
+        response.should render_template(:index)
+      end
     end
   end
 
   context "GET :show" do
-    it "should assign the requested event" do
-      controller.stubs(event: event)
+    it "assigns the event and renders the template" do
       get :show, id: event.id
-      controller.event.should eql(event)
+      controller.events.should_not be_nil
+      response.should render_template(:show)
     end
 
-    it "should render the :show template" do
-      get :show, id: event.id
-      response.should render_template(:show)
+    context "json" do
+      it "renders json" do
+        controller.stubs(event: event)
+        get :show, format: :json
+        response.headers["Content-Type"].should eql("application/json; charset=utf-8")
+        JSON.parse(response.body).keys.should include("id", "name")
+      end
+    end
+
+    context "ics" do
+      it "renders json" do
+        controller.stubs(event: event)
+        get :show, format: :ics
+        response.headers["Content-Type"].should eql("text/calendar; charset=utf-8")
+        response.body.should match("VCALENDAR")
+      end
     end
   end
 end
