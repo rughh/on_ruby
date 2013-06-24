b9ad7820558658660692674036904a9c6a2fff98
Peter Schröder <phoetmail@googlemail.com>
2013-06-23 23:34:09 -0400

    improve testcoverage and dry up the controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,6 @@
 class TopicsController < ApplicationController
   before_action :authenticate!, except: :show
+  before_action :validate_topic_ownership!, only: [:edit, :update]
 
   expose(:topic, attributes: :topic_params)
   expose(:events) { Event.with_topics.ordered.page(params[:page]).per(10) }
@@ -21,16 +22,18 @@ class TopicsController < ApplicationController
   end
 
   def update
-    if topic.user == current_user
-      if topic.save
-        redirect_to(topic_path(topic), notice: t("flash.topic_updated"))
-      else
-        redirect_to(edit_topic_path, alert: topic.errors.full_messages.join(', '))
-      end
+    if topic.save
+      redirect_to(topic_path(topic), notice: t("flash.topic_updated"))
+    else
+      redirect_to(edit_topic_path, alert: topic.errors.full_messages.join(', '))
     end
   end
 
   private
+
+  def validate_topic_ownership!
+    redirect_to(root_path, alert: t("flash.not_authenticated")) unless topic.user == current_user
+  end
 
   def topic_params
     params.require(:topic).permit(:label, :name, :user, :description)
--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -4,11 +4,36 @@ describe TopicsController do
   context "GET :show" do
     let(:topic) { create(:topic) }
 
-    it "should assign members" do
+    it "renders the action" do
       get :show, id: topic.id
 
       controller.topic.should eql(topic)
       response.should render_template(:show)
+    end
+  end
+
+  context "GET :new" do
+    let(:user) { build(:user) }
+
+    it "renders the action" do
+      controller.stubs(current_user: user)
+      get :new
+
+      controller.topic.should_not be_persisted
+      response.should render_template(:new)
+    end
+  end
+
+  context "GET :edit" do
+    let(:user) { create(:user) }
+    let(:topic) { create(:topic, user: user) }
+
+    it "renders the action" do
+      controller.stubs(current_user: user)
+      get :edit, id: topic.id
+
+      controller.topic.should be_persisted
+      response.should render_template(:edit)
     end
   end
 
@@ -17,10 +42,8 @@ describe TopicsController do
     let(:topic_data) { attributes_for(:topic) }
 
     it "should create a topic for logged-in user" do
-      @controller.stubs(current_user: user)
-      expect do
-        post(:create, topic: topic_data)
-      end.to change(Topic, :count).by(1)
+      controller.stubs(current_user: user)
+      expect { post(:create, topic: topic_data) }.to change(Topic, :count).by(1)
       controller.topic.user.should eql(user)
       flash[:notice].should_not be_nil
     end
@@ -32,20 +55,35 @@ describe TopicsController do
   end
 
   context "POST :update" do
-    let(:user) { create(:user) }
-    let(:topic) { create(:topic, user: user) }
-
-    it "should update a topic for logged-in user" do
-      @controller.stubs(current_user: user)
-      put(:update, id: topic, topic: {name: 'blupp'})
-
-      controller.topic.name.should eql('blupp')
-      flash[:notice].should_not be_nil
+    it "authenticates the action" do
+      put(:update, id: 123, topic: attributes_for(:topic))
+      response.should redirect_to(root_path)
+      flash[:alert].should_not be_nil
     end
 
-    it "should not update a topic if not signed in" do
-      put(:update, id: topic.id, topic: attributes_for(:topic))
-      response.should redirect_to(root_path)
+    context "with logged-in user" do
+      let(:user) { create(:user) }
+      let(:other_user) { create(:user) }
+      let(:topic) { create(:topic, user: user) }
+
+      before do
+        controller.stubs(current_user: user)
+      end
+
+      it "updates a topic" do
+        put(:update, id: topic, topic: {name: 'blupp'})
+
+        controller.topic.name.should eql('blupp')
+        flash[:notice].should_not be_nil
+      end
+
+      it "does not update a topic for a different user" do
+        controller.stubs(current_user: other_user)
+        put(:update, id: topic, topic: {name: 'blupp'})
+
+        expect(response).to redirect_to(root_path)
+        flash[:alert].should_not be_nil
+      end
     end
   end
 end
