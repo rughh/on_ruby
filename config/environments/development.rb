663fe89b8ca7f057dfadac52d96140cc2fef2643
Peter Schröder <phoetmail@googlemail.com>
2013-07-05 18:04:21 +0200

    adapat changes due to rake rails:update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,32 +1,32 @@
 OnRuby::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
-  # every request.  This slows down response time but is perfect for development
+  # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports and disable caching
+  # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Configure Gmail for development-mails
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :letter_opener
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method       = :letter_opener
 
-  # Print deprecation notices to the Rails logger
+  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Expands the lines which load the assets
-  config.assets.debug = false
+  # Raise an error on page load if there are pending migrations
+  config.active_record.migration_error = :page_load
 
-  # Disable logging of asset requests
-  config.assets.logger = false
-
-  # Debug RJS
-  # config.action_view.debug_rjs = true
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
 
   config.middleware.use "CookieDomain", ".onruby.dev"
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,3 @@
-OnRuby::Application.config.session_store :cookie_store, key: "_on_ruby_session"
+# Be sure to restart your server when you modify this file.
+
+OnRuby::Application.config.session_store :cookie_store, key: '_on_ruby_session'
