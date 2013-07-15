9285bd6d73602c5765a560ff3347ce2632be7058
Peter Schröder <phoetmail@googlemail.com>
2013-07-15 18:37:47 +0200

    add so_coded ribbon

--- app/helpers/external_link_helper.rb
+++ app/helpers/external_link_helper.rb
@@ -41,8 +41,9 @@ module ExternalLinkHelper
 
   def ribbon(type)
     types = {
-      github:          ["Fork me on GitHub!", "https://github.com/phoet/on_ruby"],
-      senor_developer: ["Señor Developer!",   "http://senordevelopershop.spreadshirt.de"],
+      github:           ["Fork me on GitHub!", "https://github.com/phoet/on_ruby"],
+      senor_developer:  ["Señor Developer!",   "http://senordevelopershop.spreadshirt.de"],
+      so_coded:         ["so coded",           "http://socoded.com"],
     }
     text, url = types[type]
     content_tag :div, id: "#{type}_ribbon", class: 'ribbon_wrap' do
