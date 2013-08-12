5eb573a36c0c6df148f26c1852044fefac7ee78b
Peter Schröder <phoetmail@googlemail.com>
2013-08-12 19:55:44 +0200

    add spec for disable action

---
+++ spec/controllers/admin/highlights_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Admin::HighlightsController do
+  context "as admin" do
+    before do
+      controller.stubs(current_user: build(:admin_user))
+    end
+
+    context "GET :duplicate" do
+      let(:highlight) { create(:highlight) }
+      it "duplicates the last event" do
+        get :disable, id: highlight
+        expect(assigns[:highlight]).to be_disabled
+      end
+    end
+  end
+end
