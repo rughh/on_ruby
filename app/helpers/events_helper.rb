97951eb757bb9cc363b2d4af23c4d937bc40dc92
Peter Schröder <phoetmail@googlemail.com>
2014-01-05 12:23:52 +0100

    möp, this is the right order

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,10 +1,10 @@
 module EventsHelper
   def participation_link(event)
     if signed_in?
-      if event.closed?
-        button_to t("show.attend"), "#", data: {disable: t("flash.already_closed")}
-      elsif current_user.participates?(event)
+      if current_user.participates?(event)
         button_to t("show.not_attend"), [event, current_user.participation(event)], method: :delete
+      elsif event.closed?
+        button_to t("show.attend"), "#", data: {disable: t("flash.already_closed")}
       else
         button_to t("show.attend"), [event, Participant.new]
       end
