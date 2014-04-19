f0c06fb1cfa1828c2aecc72ac3b71505f44d3e39
phoet <phoetmail@googlemail.com>
2014-04-19 03:05:53 -0400

    some more niceness for peek

--- config/initializers/peek.rb
+++ config/initializers/peek.rb
@@ -1,12 +1,14 @@
 Peek.into Peek::Views::Git, :nwo => 'phoet/on_ruby'
 Peek.into Peek::Views::PerformanceBar
-Peek.into Peek::Views::Dalli
 Peek.into Peek::Views::PG
 Peek.into Peek::Views::GC
 
-OnRuby::Application.configure do
-  config.peek.adapter = :memcache, {
-    client: Dalli::Client.new(ENV["MEMCACHIER_SERVERS"], username: ENV["MEMCACHIER_USERNAME"], password: ENV["MEMCACHIER_PASSWORD"]),
-    expires_in: 60 * 30
-  }
+if defined? Dalli
+  Peek.into Peek::Views::Dalli
+  OnRuby::Application.configure do
+    config.peek.adapter = :memcache, {
+      client: Dalli::Client.new(ENV["MEMCACHIER_SERVERS"], username: ENV["MEMCACHIER_USERNAME"], password: ENV["MEMCACHIER_PASSWORD"]),
+      expires_in: 60 * 30
+    }
+  end
 end
