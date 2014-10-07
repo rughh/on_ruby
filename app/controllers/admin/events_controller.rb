3fc76a1bd8931e128e1bb7b004042230521a0161
Martin Wilhelmi <mnin@mnin.de>
2014-10-07 16:05:17 +0200

    introducing zeropush for ios app notifications

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -7,8 +7,21 @@ class Admin::EventsController < Admin::ResourcesController
   def publish
     event = Event.find(params[:id])
     UsergroupMailer.invitation_mail(event).deliver!
+    push_app_notification(event)
     event.update_attributes! published: true
 
     redirect_to url_for(controller: "/admin/events", action: :edit, id: event.id), notice: "Published!"
+  end
+
+  private
+
+  def push_app_notification(event)
+    options = {
+      channel:           Usergroup.name.parameterize,
+      alert:             "[#{Usergroup.name}]: new event at #{I18n.l(event.date)}",
+      content_available: true
+    }
+
+    ZeroPush.broadcast(options)
   end
 end
