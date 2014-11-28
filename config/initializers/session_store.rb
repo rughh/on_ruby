56c0b8969d6484db2f639d7586c2b055d1bff947
phoet <phoetmail@googlemail.com>
2014-11-28 18:24:09 +0100

    expire all cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-OnRuby::Application.config.session_store :cookie_store, key: '_on_ruby_session'
+OnRuby::Application.config.session_store :cookie_store, key: '_on_ruby_session_v2'
