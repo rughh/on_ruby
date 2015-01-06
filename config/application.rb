6079269795bbd00a021b041cd9e3f8af8ed7ec87
kikito <kikito@gmail.com>
2015-01-06 21:03:55 +0100

    Put all the I18n config options inside the i18n namespace, and include :es

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,8 @@ module OnRuby
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.locale = config.i18n.default_locale = :de
-    config.available_locales = [:de, :en]
-    config.enforce_available_locales = true
+    config.i18n.available_locales = [:de, :en, :es]
+    config.i18n.enforce_available_locales = true
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
