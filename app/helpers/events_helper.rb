a2ac5454a97237d449544d09ea8101120e4d65b8
kikito <kikito@gmail.com>
2015-02-22 14:12:40 +0100

    Include webcal and rss links for all the events at the end of events/index
    Related with #171

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -12,4 +12,14 @@ module EventsHelper
       button_to t("show.attend"), "#", data: {disable: t("show.login_before_attend")}
     end
   end
+
+  def all_events_webcal_link
+    fa_icon('calendar-o') + ' ' +
+    link_to(t('events.webcal'), events_url(format: 'ics', protocol: 'webcal'))
+  end
+
+  def all_events_rss_link
+    fa_icon('rss') + ' ' +
+    link_to(t('events.rss'), events_url(format: 'xml'))
+  end
 end
