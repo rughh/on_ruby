7351d9689befd6fa123009dafdb39677d6d18b3b
phoet <phoetmail@googlemail.com>
2015-04-22 09:01:35 +0200

    lets see if it has something to do with puma

--- config/puma.rb
+++ config/puma.rb
@@ -11,5 +11,6 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  GC::Profiler.enable
   ActiveRecord::Base.establish_connection
 end
