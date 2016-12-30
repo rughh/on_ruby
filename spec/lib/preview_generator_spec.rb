3f9ba3f46fcd80549e03bbc75a66b2d168feaf58
phoet <phoetmail@googlemail.com>
2016-12-30 21:37:46 +0100

    no 9

--- spec/lib/preview_generator_spec.rb
+++ spec/lib/preview_generator_spec.rb
@@ -1,30 +1,27 @@
-require "spec_helper"
+require 'spec_helper'
 
-describe "PreviewGenerator" do
-
-  it "can be created by an URI" do
+describe 'PreviewGenerator' do
+  it 'can be created by an URI' do
     g = PreviewGenerator.new('http://google.com')
     expect(g.uri).to eq('http://google.com')
   end
 
-  context "#generate_preview" do
-
-    it "invokes LinkThumbnailer" do
+  context '#generate_preview' do
+    it 'invokes LinkThumbnailer' do
       website = double(videos: [], images: [])
       expect(LinkThumbnailer).to receive(:generate).with('http://hey.com').and_return(website)
 
       PreviewGenerator.new('http://hey.com').generate_preview
     end
 
-    it "handles bad URIs" do
-      expect(LinkThumbnailer).to receive(:generate).and_raise(Net::HTTPServerException.new("", ""))
+    it 'handles bad URIs' do
+      expect(LinkThumbnailer).to receive(:generate).and_raise(Net::HTTPServerException.new('', ''))
 
-      expect { PreviewGenerator.new('http://hey.com').generate_preview }.not_to raise_error      
+      expect { PreviewGenerator.new('http://hey.com').generate_preview }.not_to raise_error
     end
 
-    it "detects the first embeddable video code" do
-
-      embeddable     = double(embed_code: "code")
+    it 'detects the first embeddable video code' do
+      embeddable     = double(embed_code: 'code')
       non_embeddable = double(embed_code: nil)
       website        = double(videos: [non_embeddable, embeddable], images: [])
 
@@ -40,9 +37,8 @@ describe "PreviewGenerator" do
       expect(pg.code).to eq('code')
     end
 
-    it "uses the first available image" do
-
-      img            = double(src: "src")
+    it 'uses the first available image' do
+      img            = double(src: 'src')
       website        = double(videos: [], images: [img])
       expect(LinkThumbnailer).to receive(:generate).with('http://hey.com').and_return(website)
 
@@ -57,8 +53,7 @@ describe "PreviewGenerator" do
       expect(pg.code).to eq('src')
     end
 
-    it "gives up with no videos nor images" do
-
+    it 'gives up with no videos nor images' do
       website = double(videos: [], images: [])
       expect(LinkThumbnailer).to receive(:generate).with('http://hey.com').and_return(website)
 
@@ -69,11 +64,6 @@ describe "PreviewGenerator" do
       expect(pg.video?).to eq(false)
       expect(pg.image?).to eq(false)
       expect(pg.none?).to eq(true)
-
     end
-
   end
-
-
 end
-
