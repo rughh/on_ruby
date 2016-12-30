de1f48353eb18339d459439b662aba20f402d50f
phoet <phoetmail@googlemail.com>
2016-12-30 21:38:43 +0100

    no 10

--- spec/mailers/usergroup_mailer_spec.rb
+++ spec/mailers/usergroup_mailer_spec.rb
@@ -1,17 +1,16 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe UsergroupMailer do
-
   let(:event) { create(:event) }
 
-  context "sending a mail" do
-    it "should send the mail" do
+  context 'sending a mail' do
+    it 'should send the mail' do
       email = UsergroupMailer.invitation_mail(event).deliver_now
       expect(ActionMailer::Base.deliveries).to_not be_empty
 
-      expect(email.to).to eql(["rubyonrails-ug-germany@googlegroups.com"])
+      expect(email.to).to eql(['rubyonrails-ug-germany@googlegroups.com'])
       expect(email.subject).to match(event.name)
-      expect(email.encoded).to match(/<h1>#{event.name}<\/h1>/)
+      expect(email.encoded).to match(%r{<h1>#{event.name}</h1>})
     end
   end
 end
