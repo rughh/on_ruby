188a13787ae082cc84ce5eb0c14989a71663a561
phoet <phoetmail@googlemail.com>
2016-12-30 21:35:32 +0100

    no 8

--- spec/helpers/events_helper_spec.rb
+++ spec/helpers/events_helper_spec.rb
@@ -5,35 +5,35 @@ describe EventsHelper do
   let(:participant) { create(:participant, user: event.user) }
   let(:participation_event) { create(:event, participants: [participant]) }
 
-  context "signed_out user" do
+  context 'signed_out user' do
     before(:each) do
       allow(helper).to receive_messages(signed_in?: false, current_user: nil)
     end
 
-    context "#participation_link" do
-      it "should have a login message attached" do
-        expect(helper.participation_link(event)).to match("Bitte logge dich zuerst ein")
+    context '#participation_link' do
+      it 'should have a login message attached' do
+        expect(helper.participation_link(event)).to match('Bitte logge dich zuerst ein')
       end
     end
   end
 
-  context "signed_in user" do
+  context 'signed_in user' do
     before(:each) do
       allow(helper).to receive_messages(signed_in?: true, current_user: event.user)
     end
 
-    context "#participation_link" do
-      it "should render a paritcipate button" do
-        expect(helper.participation_link(event)).to match("Teilnehmen")
+    context '#participation_link' do
+      it 'should render a paritcipate button' do
+        expect(helper.participation_link(event)).to match('Teilnehmen')
       end
 
-      it "disbales the button on closed events" do
+      it 'disbales the button on closed events' do
         event = build(:closed_event)
-        expect(helper.participation_link(event)).to match("data-disable")
+        expect(helper.participation_link(event)).to match('data-disable')
       end
 
-      it "should render a cancel button" do
-        expect(helper.participation_link(participation_event)).to match("Absagen")
+      it 'should render a cancel button' do
+        expect(helper.participation_link(participation_event)).to match('Absagen')
       end
     end
   end
