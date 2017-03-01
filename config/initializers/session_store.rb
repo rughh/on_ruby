09afd035bd6294bc639c78171d4b821ab1d05146
phoet <phoetmail@googlemail.com>
2017-03-01 11:30:47 +0100

    try again cookie store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-if Rails.env.production?
-  Rails.application.config.session_store :redis_store, servers: "#{ENV['REDIS_URL']}/0/session", key: '_on_ruby_session'
-else
-  Rails.application.config.session_store :cookie_store, key: '_on_ruby_session'
-end
+Rails.application.config.session_store :cookie_store, key: '_rsid'
