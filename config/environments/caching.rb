dbb7ecb9f9c5ab9874953695ad4a0e2d06b44553
phoet <phoetmail@googlemail.com>
2017-03-01 20:06:45 +0100

    Revert "use mailgun"
    This reverts commit 16a5e9f3337e8b86fdaf1a9bdfdbe27b17ba8a08.

--- config/environments/caching.rb
+++ config/environments/caching.rb
@@ -62,12 +62,12 @@ OnRuby::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    port:           ENV['MAILGUN_SMTP_PORT'],
-    address:        ENV['MAILGUN_SMTP_SERVER'],
-    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
-    password:       ENV['MAILGUN_SMTP_PASSWORD'],
-    domain:         'heroku.com',
-    authentication: :plain,
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
   }
 
   # Enable threaded mode
