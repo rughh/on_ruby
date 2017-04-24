5132133d6ab9cd894d75d79dab8b621299bda3d9
Jan Zaydowicz <jan.zaydowicz@googlemail.com>
2017-04-24 23:45:53 +0200

    Upgrading to Formtastic 3.1
    remove the i18n look up setting as it is now the default.
    Add the required namespace settings

--- config/initializers/formtastic.rb
+++ config/initializers/formtastic.rb
@@ -1,1 +1,11 @@
-Formtastic::FormBuilder.i18n_lookups_by_default = true
+# By creating custom input class finder, you can change how input classes  are looked up.
+# For example you can make it to search for TextInputFilter instead of TextInput.
+# See # TODO: add link # for more information
+# NOTE: this behavior will be default from Formtastic 4.0
+Formtastic::FormBuilder.input_class_finder = Formtastic::InputClassFinder
+
+# By creating custom action class finder, you can change how action classes are looked up.
+# For example you can make it to search for MyButtonAction instead of ButtonAction.
+# See # TODO: add link # for more information
+# NOTE: this behavior will be default from Formtastic 4.0
+Formtastic::FormBuilder.action_class_finder = Formtastic::ActionClassFinder
