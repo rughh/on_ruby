aa19ab7b5b8f56e6424a5e0d36d8920002d0aa04
phoet <phoetmail@googlemail.com>
2018-06-08 10:33:38 +0200

    see if prepending the filter works

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,13 @@
 class Admin::ApplicationController < Admin::ResourcesController
   include WhitelabelDetection
 
-  before_action :switch_label
+  prepend_before_action :debug_label
+
+  private
+
+  def debug_label
+    Rails.logger.info("setting label")
+    switch_label
+    Rails.logger.info("setting label to #{Whitelabel[:label_id]}")
+  end
 end
