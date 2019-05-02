56fff99ca0a5a2e7ccdbd570a94b160c32c744ce
phoet <phoetmail@googlemail.com>
2019-05-02 17:26:27 +0200

    let it run

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.hosts << "hamburg.onruby.test"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
