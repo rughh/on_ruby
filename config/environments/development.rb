b9f33f3e86856a0236bda839566c47c6e2ebdcfa
phoet <phoetmail@googlemail.com>
2019-05-02 20:24:40 +0200

    merge settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
-  config.hosts << "hamburg.onruby.test"
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "hamburg.onruby.test"
+  config.middleware.use CookieDomain, ".onruby.test"
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -17,6 +18,7 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -28,7 +30,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
+  # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
@@ -53,12 +55,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  config.middleware.use CookieDomain, ".onruby.test"
 end
