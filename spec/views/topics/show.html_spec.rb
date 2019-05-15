b51647c92393175883391e5d3cbe884ef80e21b5
phoet <phoetmail@googlemail.com>
2019-05-15 22:10:18 +0200

    remove likes

--- spec/views/topics/show.html_spec.rb
+++ spec/views/topics/show.html_spec.rb
@@ -4,7 +4,7 @@ describe 'topics/show' do
   let(:topic) { build(:topic) }
 
   it 'should render successfully' do
-    allow(view).to receive_messages(topic: topic, signed_in?: false, likes: '</>')
+    allow(view).to receive_messages(topic: topic, signed_in?: false)
 
     render
   end
