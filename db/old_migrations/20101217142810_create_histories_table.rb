e9b7942b7862e3a8244e3ea547fb657a3ea89dbd
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2019-10-07 19:50:16 +0200

    Fix `Layout/BlockAlignment` offences

--- db/old_migrations/20101217142810_create_histories_table.rb
+++ db/old_migrations/20101217142810_create_histories_table.rb
@@ -8,7 +8,7 @@ class CreateHistoriesTable < ActiveRecord::Migration
        t.integer :month, :limit => 2
        t.integer :year, :limit => 5
        t.timestamps
-    end
+     end
     add_index(:histories, [:item, :table, :month, :year], name: 'index_histories_on_item_and_date')
   end
 
