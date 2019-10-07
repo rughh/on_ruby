899fbc1688f363aa0b4b328ff867b0aef7b09383
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2019-10-07 20:19:43 +0200

    Eliminate `Layout/InitialIndentation` offences

--- app/models/concerns/slug.rb
+++ app/models/concerns/slug.rb
@@ -1,41 +1,41 @@
-  module Slug
-    def self.included(clazz)
-      def clazz.from_param(token)
-        id = if match = token.match(/.+-(\d+)/)
-               match[1]
-             else
-               token
-        end
-        found = where(id: id.to_i).or(where("#{table_name}.#{slugger} ILIKE ?", token.tr('-', '%'))).first
-        raise ActiveRecord::RecordNotFound.new("Could not find by slug #{token}") unless found
-
-        found
+module Slug
+  def self.included(clazz)
+    def clazz.from_param(token)
+      id = if match = token.match(/.+-(\d+)/)
+             match[1]
+           else
+             token
       end
+      found = where(id: id.to_i).or(where("#{table_name}.#{slugger} ILIKE ?", token.tr('-', '%'))).first
+      raise ActiveRecord::RecordNotFound.new("Could not find by slug #{token}") unless found
 
-      def clazz.find_by_slug(token)
-        from_param(token)
-      end
-
-      def clazz.slugged_by(column)
-        @slugger = column
-      end
-
-      def clazz.slugger
-        @slugger || :name
-      end
+      found
     end
 
-    def slug
-      self.send(self.class.slugger).parameterize
+    def clazz.find_by_slug(token)
+      from_param(token)
     end
 
-    def to_param
-      "#{slug}-#{id}"
+    def clazz.slugged_by(column)
+      @slugger = column
     end
 
-    def as_json(options = {})
-      h = super(options)
-      h[:slug] = slug
-      h
+    def clazz.slugger
+      @slugger || :name
     end
+  end
+
+  def slug
+    self.send(self.class.slugger).parameterize
+  end
+
+  def to_param
+    "#{slug}-#{id}"
+  end
+
+  def as_json(options = {})
+    h = super(options)
+    h[:slug] = slug
+    h
+  end
 end
