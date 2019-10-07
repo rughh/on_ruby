69c764122409674e5451948c487bae7b208b1f4b
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2019-10-07 20:32:34 +0200

    Eliminate `Style/BarePercentLiterals` offences

--- app/mailers/usergroup_mailer.rb
+++ app/mailers/usergroup_mailer.rb
@@ -2,7 +2,7 @@ class UsergroupMailer < ActionMailer::Base
   def invitation_mail(event)
     @event = event
     options = {
-      from: %Q("#{I18n.tw('name')}" <onruby@googlemail.com>),
+      from: %("#{I18n.tw('name')}" <onruby@googlemail.com>),
       to: "rubyonrails-ug-germany@googlegroups.com",
       subject: "#{Whitelabel[:label_id].upcase} Info: #{event.name}",
     }
