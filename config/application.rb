d291b38082d8861eacea722a44a845735c2f7f3f
Andreas Finger <webmaster@mediafinger.com>
2019-10-15 22:07:11 +0200

    Update to Rails 6.0.0 only load the frameworks used

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,22 @@
-require_relative 'boot'
+# frozen_string_literal: true
 
-require 'rails/all'
+require_relative "boot"
+
+require "rails"
+
+# Pick the frameworks you want:
+require "active_model/railtie"
+# require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -34,9 +50,9 @@ module OnRuby
     # Configure generators.
     config.generators do |g|
       g.template_engine :slim
-      g.test_framework :rspec, :fixture => true, :views => false
-      g.integration_tool :rspec, :fixture => true, :views => true
-      g.fixture_replacement :factory_girl, :dir => "spec/support/factories"
+      g.test_framework :rspec, fixture: true, views: false
+      g.integration_tool :rspec, fixture: true, views: true
+      g.fixture_replacement :factory_bot, dir: "spec/support/factories"
       g.stylesheet_engine = :sass
     end
   end
