2e3f550590bda22d2d832d5169cf8a183d8e7d59
Andreas Finger <webmaster@mediafinger.com>
2019-10-15 20:59:30 +0200

    Update to Rails 6.0.0 new framework defaults

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,11 @@ Bundler.require(*Rails.groups)
 module OnRuby
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # -- all .rb files in that directory are automatically loaded after loading Rails and any gems.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
