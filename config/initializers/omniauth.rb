d543aabf24ac76941226fbfc015b0f277e79ae09
phoet <phoetmail@googlemail.com>
2019-10-15 18:21:21 +0200

    fix root domain mappings

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, {
     setup: Proc.new do |env|
       host = env['SERVER_NAME']
-      name = "OMNIAUTH_GITHUB_#{host[/.+\.(.+\..+)/, 1].delete('-').split('.').join('_').upcase}"
+      tld = host[/(.+\.)?(.+\..+)/, 2]
+      token = tld.delete('-').split('.').join('_').upcase
+      name = "OMNIAUTH_GITHUB_#{token}"
 
       env['omniauth.strategy'].options[:client_id]     = ENV["#{name}_KEY"]
       env['omniauth.strategy'].options[:client_secret] = ENV["#{name}_SECRET"]
