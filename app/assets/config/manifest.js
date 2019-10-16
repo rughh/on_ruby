a027690f3837e2b9c4ff52180f1796fb4bae6275
Andreas Finger <webmaster@mediafinger.com>
2019-10-16 10:51:35 +0200

    Update to Rails 6.0.0 add manifest.js and add assets to precompile

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,10 @@
 // Rails 6 / Webpacker insist on this manifest file to
 // not throw a Sprockets::Railtie::ManifestNeededError
-
+//
+// app/assets/config/manifest.js
+//
 //= link_tree ../images
-//= link_tree ../javascripts .js
-//= link_tree ../stylesheets .sass
+//= link application.js
+//= link administrate/application.js
+//= link application.css
+//= link administrate/application.css
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,15 +1,32 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w(labels/*)
+
+# since updating to Rails 6 and adding webpacker, the files need explicit listing
+# there should be a better solution
+Rails.application.config.assets.precompile += %w(
+  labels/cologne.js
+  application.js
+
+  labels/barcelona.css
+  labels/bonn.css
+  labels/cologne.css
+  labels/hamburg.css
+  labels/leipzig.css
+  labels/madridrb.css
+  labels/munich.css
+  labels/railsgirlshh.css
+  labels/saar.css
+  application.css
+)
