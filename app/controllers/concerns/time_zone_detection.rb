6f43a5af12ccbb716ef5adfb6d03f9d567a2adcc
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2019-10-17 10:50:56 +0200

    Use ternary operation in time zone detection

--- app/controllers/concerns/time_zone_detection.rb
+++ app/controllers/concerns/time_zone_detection.rb
@@ -6,11 +6,7 @@ module TimeZoneDetection
   def switch_time_zone
     time_zone = params[:time_zone]
 
-    Time.zone = if allowed_time_zone?(time_zone)
-                  time_zone
-                else
-                  default_time_zone
-                end
+    Time.zone = allowed_time_zone?(time_zone) ? time_zone : default_time_zone
   end
 
   def allowed_time_zone?(time_zone)
