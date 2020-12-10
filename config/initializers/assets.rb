e6c556083c8b47b9b70da60b42c68ba139f8f3e4
Peter Schröder <phoetmail@googlemail.com>
2020-12-10 14:53:18 +0100

    remake the label page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w[labels/*]
+Rails.application.config.assets.precompile << 'application_tailwind'
