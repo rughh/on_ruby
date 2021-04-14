ea8d9a868ff7918f02dbf7dd893a0ed87d6db8dc
Paul Mölders <paulmoelders@gmail.com>
2021-04-14 11:46:27 +0200

    update preview image and properties

--- app/helpers/share_helper.rb
+++ app/helpers/share_helper.rb
@@ -6,20 +6,14 @@ module ShareHelper
 
     params = {
       :s => image_url("/images/onruby_share.svg"),
-      :title => page_title,
-      :subtitle => "#{I18n.t("show.hosted_by")} #{event.user.name}",
-      :date => "#{I18n.l(event.date, format: :long)}",
+      :title => title,
+      :subtitle => subtitle,
+      :host => "#{I18n.l(event.date, format: :long)}, Host: #{I18n.t("show.hosted_by")} #{event.user.name}",
       :location => event.location ? "#{I18n.t('show.at')} #{event.location.name}, #{event.location.address}" : "",
-      :participants => event.participants.count > 1 ? event.participants.count : " ",
+      :participants => event.participants.count > 1 ? I18n.t("show.attendees", count: event.limit || 0, participant_count: event.participants.count) : " ",
       :logo => image_url("labels/#{event.label}.png")
     }
 
     return "https://p.salmo.link/render.png?#{params.to_query}"
-  end
-
-  private 
-
-  def share_event_details
-    "#{I18n.t("show.hosted_by")} #{event.user.name} #{I18n.t('show.at')} #{event.location}"
   end
 end
