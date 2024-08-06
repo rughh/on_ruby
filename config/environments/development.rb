1c160053e77260bec73eb7043473974e7f20386b
Ben Rexin <ben@rexin.at>
2024-08-06 23:17:48 +0200

    chore: replace /etc/hosts change with RFC6761 localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << /.*onruby\.test.*/
+  config.hosts << /.*onruby\.localhost.*/
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
