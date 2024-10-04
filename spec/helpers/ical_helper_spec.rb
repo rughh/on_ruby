6d6da92f25df71a55cba8a630030eb2bec56e1b4
Ben Rexin <ben@rexin.at>
2024-10-04 22:16:44 +0200

    fix: correct ical start/end times

--- spec/helpers/ical_helper_spec.rb
+++ spec/helpers/ical_helper_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe IcalHelper do
-  let(:event) { build(:event, id: 123, name: 'awesome ottawa', date: '11.11.2011 19:00 UTC') }
+  let(:event) { build(:event, id: 123, name: 'awesome ottawa', date: '11.11.2011 19:00 +02:00') }
 
   it 'creates a valid icalendar event' do
     helper.icalendar(event).to_s.tap do |it|
-      expect(it).to match('BEGIN:VCALENDAR')
       expect(it).to match('http://test.host/events/awesome-ottawa-123')
       expect(it).to match('SUMMARY:awesome ottawa')
+      expect(it).to match('DTSTART:20111111T170000')
     end
   end
 
