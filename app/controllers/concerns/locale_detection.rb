eda92b421cd8a316b45d9adaa1e412decf8e9d37
Ben Rexin <ben@rexin.at>
2024-10-08 00:17:10 +0200

    feat: detect browser locale

--- app/controllers/concerns/locale_detection.rb
+++ app/controllers/concerns/locale_detection.rb
@@ -4,7 +4,7 @@ module LocaleDetection
   protected
 
   def switch_locale
-    locale = params[:locale] || cookies[:locale]
+    locale = params[:locale] || cookies[:locale] || available_accepted_locales
     I18n.locale = allowed_locale?(locale) ? locale : default_locale
 
     cookies[:locale] = {
@@ -20,5 +20,24 @@ module LocaleDetection
 
   def default_locale
     Whitelabel.label ? Whitelabel[:default_locale] : I18n.default_locale
+  end
+
+  def available_accepted_locales
+    (accepted_locales & I18n.available_locales).first
+  end
+
+  def accepted_locales
+    locales = parsed_accepted_locales.sort_by(&:last).reverse
+    locales.map(&:first)
+  end
+
+  def parsed_accepted_locales
+    accepted_locales = request.env['HTTP_ACCEPT_LANGUAGE']&.split(',')
+    return [] unless accepted_locales
+
+    accepted_locales.map do |prefered_locale|
+      locale, quality = prefered_locale.strip.split(/;(?:q=)/)
+      [locale.strip.to_sym, quality&.to_f || 1.0]
+    end
   end
 end
