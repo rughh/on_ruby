5c3a93bfb09ceb014a534fe5f907705e09402af0
Ben Rexin <ben@rexin.at>
2025-01-22 08:59:29 +0100

    fix: replace global usergroup with temporary to remove colognerb reference

--- spec/requests/whitelabel_spec.rb
+++ spec/requests/whitelabel_spec.rb
@@ -1,6 +1,23 @@
 require 'spec_helper'
 
 describe 'Whitelabel' do
+  let(:hamburg) { Usergroup.from_name("hamburg") }
+  let(:berlin) { Usergroup.from_name("berlin") }
+  let(:tokio) { Usergroup.from_name("tokio") }
+
+  around do |example|
+    whitelabel_backup = Whitelabel.labels
+    Whitelabel.labels = [
+      hamburg,
+      berlin,
+      tokio
+    ]
+    example.run
+  ensure
+    Whitelabel.labels = whitelabel_backup
+  end
+
+
   context 'GET label page with non existing subdomain' do
     it 'does not do an endless redirect but halts' do
       host! 'www.onruby.test'
@@ -18,11 +35,11 @@ describe 'Whitelabel' do
 
   context 'GET page with custom domain' do
     it 'shows the label' do
-      host! 'www.colognerb.de'
+      host! 'www.tokio.de'
 
       get root_url
       expect(response).to be_a_successful
-      expect(Whitelabel[:label_id]).to eql('cologne')
+      expect(Whitelabel[:label_id]).to eql(tokio.label_id)
     end
   end
 end
