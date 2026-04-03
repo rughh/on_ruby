b2d36b3da362af7835b7200256c1f3f4bb11ae69
Ben Rexin <ben@rexin.at>
2026-04-03 16:34:02 +0200

    chore: upgrade to rails 7.0

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,3 @@
+//= link_tree ../images
+//= link_tree ../javascripts .js
+//= link_tree ../stylesheets .css
--- spec/requests/public_pages_spec.rb
+++ spec/requests/public_pages_spec.rb
@@ -1,85 +1,73 @@
 require 'spec_helper'
 
 describe 'Public pages smoke test' do
-  let!(:event) { create(:event) }
-  let!(:topic) { create(:topic, event:) }
-  let!(:user)  { event.user }
+  Whitelabel.labels.each do |whitelabel|
+    context "whitelabel: #{whitelabel.label_id}" do
+      before { host! "#{whitelabel.label_id}.onruby.test" }
 
-  context 'GET /' do
-    it 'renders the home page' do
-      get '/'
-      expect(response).to be_ok
-    end
-  end
+      let!(:location) { Whitelabel.with_label(whitelabel) { create(:location, label: whitelabel.label_id) } }
+      let!(:event)    { Whitelabel.with_label(whitelabel) { create(:event, location:) } }
+      let!(:topic)    { Whitelabel.with_label(whitelabel) { create(:topic, event:) } }
 
-  context 'GET /events' do
-    it 'renders the events index' do
-      get events_path
-      expect(response).to be_ok
-    end
+      it 'renders /' do
+        get '/'
+        expect(response).to be_ok
+      end
 
-    it 'renders as json' do
-      get events_path(format: :json)
-      expect(response).to be_ok
-    end
+      it 'renders /events' do
+        get events_path
+        expect(response).to be_ok
+      end
 
-    it 'renders as xml' do
-      get events_path(format: :xml)
-      expect(response).to be_ok
-    end
-  end
+      it 'renders /events as json' do
+        get events_path(format: :json)
+        expect(response).to be_ok
+      end
 
-  context 'GET /events/:id' do
-    it 'renders the event page' do
-      get event_path(event)
-      expect(response).to be_ok
-    end
+      it 'renders /events as xml' do
+        get events_path(format: :xml)
+        expect(response).to be_ok
+      end
 
-    it 'renders as ics' do
-      get event_path(event, format: :ics)
-      expect(response).to be_ok
-    end
-  end
+      it 'renders /events/:id' do
+        get event_path(event)
+        expect(response).to be_ok
+      end
 
-  context 'GET /topics' do
-    it 'renders the topics index' do
-      get topics_path
-      expect(response).to be_ok
-    end
-  end
+      it 'renders /events/:id as ics' do
+        get event_path(event, format: :ics)
+        expect(response).to be_ok
+      end
 
-  context 'GET /topics/:id' do
-    it 'renders a topic page' do
-      get topic_path(topic)
-      expect(response).to be_ok
-    end
-  end
+      it 'renders /topics' do
+        get topics_path
+        expect(response).to be_ok
+      end
 
-  context 'GET /users' do
-    it 'renders the users index' do
-      get users_path
-      expect(response).to be_ok
-    end
-  end
+      it 'renders /topics/:id' do
+        get topic_path(topic)
+        expect(response).to be_ok
+      end
 
-  context 'GET /users/:id' do
-    it 'renders a user page' do
-      get user_path(user)
-      expect(response).to be_ok
-    end
-  end
+      it 'renders /users' do
+        get users_path
+        expect(response).to be_ok
+      end
 
-  context 'GET /locations' do
-    it 'renders the locations index' do
-      get locations_path
-      expect(response).to be_ok
-    end
-  end
+      it 'renders /users/:id' do
+        get user_path(event.user)
+        expect(response).to be_ok
+      end
 
-  context 'GET /locations/:id' do
-    it 'renders a location page' do
-      get location_path(event.location)
-      expect(response).to be_ok
+      it 'renders /locations' do
+        get locations_path
+        expect(response).to be_ok
+      end
+
+      it 'renders /locations/:id' do
+        get location_path(event.location)
+        expect(response).to be_ok
+      end
     end
   end
 end
