dceb90daa9b693837a1d591cd41629d5d231bf1a
Ben Rexin <ben@rexin.at>
2026-04-03 18:09:24 +0200

    chore: upgrade to rails 7.2

--- app/lib/email_auth_token.rb
+++ app/lib/email_auth_token.rb
@@ -7,7 +7,7 @@ class EmailAuthToken
   def self.generate(
     email,
     expiration: 15.minutes,
-    secret: Rails.application.secrets.secret_key_base
+    secret: Rails.application.secret_key_base
   )
     now_seconds = Time.now.to_i
     payload = { iss: email, iat: now_seconds, exp: now_seconds + expiration }
@@ -15,7 +15,7 @@ class EmailAuthToken
     Base64.encode64(token)
   end
 
-  def self.decode(string, secret: Rails.application.secrets.secret_key_base)
+  def self.decode(string, secret: Rails.application.secret_key_base)
     token = Base64.decode64(string)
     JWT.decode(token, secret, true, algorithm: 'HS256').first
   end
