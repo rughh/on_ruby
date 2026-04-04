6b85459be95d6653f13aed6b677eeb0a600ec9c9
Ben Rexin <ben@rexin.at>
2026-04-04 14:54:14 +0200

    fix: mark ugc links as nofollow

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -119,6 +119,10 @@ module ApplicationHelper
   end
 
   def markdown_parser
-    @markdown_parser ||= Redcarpet::Markdown.new Redcarpet::Render::Safe, autolink: true, space_after_headers: true
+    @markdown_parser ||= Redcarpet::Markdown.new markdown_renderer, autolink: true, space_after_headers: true
+  end
+
+  def markdown_renderer
+    @markdown_renderer ||= Redcarpet::Render::Safe.new(no_styles: true, filter_html: true, link_attributes: { rel: 'nofollow noopener noreferrer ugc' })
   end
 end
