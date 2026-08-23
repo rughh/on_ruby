413367912b5e92a1ae0f7c95e016cf473b23863d
Ben Rexin <ben@rexin.at>
2026-08-23 21:48:27 +0200

    refa: replace factory_girl with factory_bot

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module OnRuby
       g.template_engine :slim
       g.test_framework :rspec, fixture: true, views: false
       g.integration_tool :rspec, fixture: true, views: true
-      g.fixture_replacement :factory_girl, dir: 'spec/support/factories'
+      g.fixture_replacement :factory_bot, dir: 'spec/support/factories'
       g.stylesheet_engine = :sass
     end
   end
