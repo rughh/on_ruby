5c4e6e3c882cb2f3b310b9efba49198372da4a4c
Ben Rexin <ben@rexin.at>
2026-08-23 22:54:13 +0200

    chore: handle invalid csrf token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,11 @@ class ApplicationController < ActionController::Base
 
   helper_method :signed_in?, :current_user
 
-  rescue_from ActiveRecord::RecordNotFound,    with: :_404
-  rescue_from ActionView::MissingTemplate,     with: :_404
+  rescue_from ActiveRecord::RecordNotFound, with: :_404
+  rescue_from ActionView::MissingTemplate, with: :_404
   rescue_from ActionController::UnknownFormat, with: :_404
-  rescue_from ActionController::BadRequest,    with: :_400
+  rescue_from ActionController::BadRequest, with: :_400
+  rescue_from ActionController::InvalidAuthenticityToken, with: :_422
 
   expose(:jobs)       { Job.shuffled }
   expose(:highlights) { Highlight.active }
@@ -29,6 +30,10 @@ class ApplicationController < ActionController::Base
   def _400(exception)
     Rails.logger.warn "400 bad_request: #{exception.message} — #{request.method} #{request.path} (#{request.remote_ip})"
     head(:bad_request)
+  end
+
+  def _422
+    head(:unprocessable_entity)
   end
 
   def _404(exception)
