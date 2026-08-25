61a1d3037f7aa41e46e3e55c2ed3a4461cc08126
Andreas Finger <andy@mediafinger.com>
2026-08-25 21:35:58 +0200

    Add the ActiveRecord version to the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_033955) do
-
+ActiveRecord::Schema[7.2].define(version: 2021_01_13_033955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -147,5 +146,4 @@ ActiveRecord::Schema.define(version: 2021_01_13_033955) do
     t.string "linkedin"
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
-
 end
