46025fd38c6457ba0ae9e5f41fefe81e2e16c240
Ben Rexin <ben@rexin.at>
2026-09-20 23:34:42 +0200

    feat: participation and topic counts as single query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,11 @@
 class HomeController < ApplicationController
   expose(:current_event)    { Event.current.first }
   expose(:events)           { Event.latest.limit(10) }
-  expose(:people)           { User.peers.limit(3 * 5) }
+  expose(:people)           { User.peers.with_counts.limit(3 * 5) }
   expose(:undone_topics)    { Topic.ordered.undone }
   expose(:upcoming_topics)  { Topic.ordered.upcoming }
   expose(:done_topics)      { Topic.ordered.done.limit(10) }
-  expose(:organizers)       { User.organizers }
+  expose(:organizers)       { User.organizers.with_counts }
   expose(:locations)        { Location.joins(:events).merge(Event.where(date: (2.years.ago.beginning_of_year)..)).distinct }
   expose(:zoom)             { Whitelabel[:location][:zoom] }
 
